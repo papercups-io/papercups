56f7cc94b60839b891487dcedeb17e4ee59a99b8
reichert621 <reichertjalex@gmail.com>
2020-07-16 11:41:26 -0400

    Minor cleanup

--- assets/src/components/ConversationsContainer.tsx
+++ assets/src/components/ConversationsContainer.tsx
@@ -323,7 +323,6 @@ class ConversationsContainer extends React.Component<Props, State> {
       conversationsById = {},
       messagesByConversation = {},
     } = this.state;
-    console.log(this.state);
 
     // TODO: add loading state
 
