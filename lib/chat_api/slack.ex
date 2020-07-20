18faeb22f4ccea8fc45c45273becbdb0975ad074
reichert621 <reichertjalex@gmail.com>
2020-07-20 13:32:58 -0400

    Fix validation of slack access token env variable

--- lib/chat_api/slack.ex
+++ lib/chat_api/slack.ex
@@ -32,8 +32,6 @@ defmodule ChatApi.Slack do
   end
 
   def send_conversation_message_alert(conversation_id, text) do
-    # TODO: handle this in user settings, for now just toggling on/off with env
-    slack_enabled = System.get_env("SLACK_BOT_ACCESS_TOKEN")
     thread = SlackConversationThreads.get_thread_by_conversation_id(conversation_id)
 
     base =
@@ -67,7 +65,9 @@ defmodule ChatApi.Slack do
         }
       end
 
-    if slack_enabled do
+    # TODO: handle this in user settings, for now just toggling on/off
+    # based on whether a valid access token is provided as an env variable
+    if slack_enabled?() do
       {:ok, response} = send_message(payload)
 
       # TODO: clean up a bit
@@ -122,6 +122,17 @@ defmodule ChatApi.Slack do
       }
     else
       raise "chat.postMessage returned ok=false"
+    end
+  end
+
+  defp slack_enabled? do
+    token = System.get_env("SLACK_BOT_ACCESS_TOKEN")
+
+    case token do
+      "xoxb-" <> _rest -> true
+      "" -> false
+      nil -> false
+      _ -> raise("Expected Slack access token to match format: xoxb-xxxxx-xxxxx-xxxxxxx")
     end
   end
 end
