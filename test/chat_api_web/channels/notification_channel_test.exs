bc24eca1b0293b8e6e8fe768e48f6801cb357870
reichert621 <reichertjalex@gmail.com>
2020-07-21 13:13:39 -0400

    Fix notification channel test

--- test/chat_api_web/channels/notification_channel_test.exs
+++ test/chat_api_web/channels/notification_channel_test.exs
@@ -1,17 +1,20 @@
 defmodule ChatApiWeb.NotificationChannelTest do
   use ChatApiWeb.ChannelCase
 
-  alias ChatApi.Accounts
+  alias ChatApi.{Accounts, Conversations}
 
   setup do
     {:ok, account} = Accounts.create_account(%{company_name: "Taro"})
+
+    {:ok, conversation} =
+      Conversations.create_conversation(%{account_id: account.id, status: "open"})
 
     {:ok, _, socket} =
       ChatApiWeb.UserSocket
       |> socket("user_id", %{some: :assign})
       |> subscribe_and_join(ChatApiWeb.NotificationChannel, "notification:lobby")
 
-    %{socket: socket, account: account}
+    %{socket: socket, account: account, conversation: conversation}
   end
 
   test "ping replies with status ok", %{socket: socket} do
@@ -19,10 +22,15 @@ defmodule ChatApiWeb.NotificationChannelTest do
     assert_reply ref, :ok, %{"hello" => "there"}
   end
 
-  test "shout broadcasts to notification:lobby", %{socket: socket, account: account} do
-    msg = %{body: "Hello world!", account_id: account.id}
+  test "shout broadcasts to notification:lobby", %{
+    socket: socket,
+    account: account,
+    conversation: conversation
+  } do
+    msg = %{body: "Hello world!", account_id: account.id, conversation_id: conversation.id}
     push(socket, "shout", msg)
-    assert_broadcast "shout", msg
+
+    assert_push("shout", msg)
   end
 
   test "broadcasts are pushed to the client", %{socket: socket} do
