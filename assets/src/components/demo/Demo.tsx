71960be994c681ed8482f2cf39ddfd8773798e38
reichert621 <reichertjalex@gmail.com>
2020-07-22 12:27:52 -0400

    Allow preloaded defaults in demo

--- assets/src/components/demo/Demo.tsx
+++ assets/src/components/demo/Demo.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {RouteComponentProps} from 'react-router-dom';
 import {Box} from 'theme-ui';
 import {TwitterPicker} from 'react-color';
+import qs from 'query-string';
 import {colors, Input, Paragraph, Title} from '../common';
 // Testing widget in separate package
 import ChatWidget from '@papercups-io/chat-widget';
 
-type Props = {};
+type Props = RouteComponentProps & {};
 type State = {
   color: string;
   title: string;
@@ -14,12 +16,22 @@ type State = {
 };
 
 class Demo extends React.Component<Props, State> {
-  state: State = {
-    color: colors.primary,
-    title: 'Welcome to Papercups!',
-    subtitle: 'Ask us anything in the chat window below 😊',
-    accountId: 'eb504736-0f20-4978-98ff-1a82ae60b266',
-  };
+  constructor(props: Props) {
+    super(props);
+
+    const q = qs.parse(props.location.search) || {};
+    const defaultTitle = q.title ? String(q.title).trim() : null;
+    const defaultSubtitle = q.subtitle ? String(q.subtitle).trim() : null;
+    const defaultColor = q.color ? String(q.color).trim() : null;
+
+    this.state = {
+      color: defaultColor || colors.primary,
+      title: defaultTitle || 'Welcome to Papercups!',
+      subtitle:
+        defaultSubtitle || 'Ask us anything in the chat window below 😊',
+      accountId: 'eb504736-0f20-4978-98ff-1a82ae60b266',
+    };
+  }
 
   handleChangeTitle = (e: any) => {
     this.setState({title: e.target.value});
@@ -73,7 +85,9 @@ class Demo extends React.Component<Props, State> {
         </Box>
 
         <Box mb={4}>
-          <Paragraph>Try changing the color:</Paragraph>
+          <Paragraph>
+            Try changing the color (hint: you can enter any hex value you want!)
+          </Paragraph>
           <TwitterPicker
             color={this.state.color}
             onChangeComplete={this.handleChangeColor}
