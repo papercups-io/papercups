1cb8b6fb3466b6442c1a57159840e6d293056c7b
reichert621 <reichertjalex@gmail.com>
2020-08-02 17:15:03 -0400

    Set up logrocket

--- assets/src/index.tsx
+++ assets/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/react';
+import LogRocket from 'logrocket';
 import './index.css';
 import App from './App';
 import {AuthProvider} from './components/auth/AuthProvider';
@@ -8,6 +9,10 @@ import * as serviceWorker from './serviceWorker';
 
 if (process.env.REACT_APP_SENTRY_DSN) {
   Sentry.init({dsn: process.env.REACT_APP_SENTRY_DSN});
+}
+
+if (process.env.REACT_APP_LOGROCKET_ID) {
+  LogRocket.init('uda6eb/papercups');
 }
 
 ReactDOM.render(
