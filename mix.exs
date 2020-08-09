a88fcdaf67cfb89350249cf80c858acf4132601d
reichert621 <reichertjalex@gmail.com>
2020-08-08 20:49:29 -0400

    Update elixir version in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ChatApi.MixProject do
     [
       app: :chat_api,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
