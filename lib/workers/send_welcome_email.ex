be45e6a24ed52e76ad9c85d44c4583dc3397cbeb
reichert621 <reichertjalex@gmail.com>
2020-08-21 17:42:26 -0400

    Small fix

--- lib/workers/send_welcome_email.ex
+++ lib/workers/send_welcome_email.ex
@@ -7,7 +7,9 @@ defmodule ChatApi.Workers.SendWelcomeEmail do
   def perform(%Oban.Job{args: %{"email" => email}}) do
     # Only send the welcome email on the hosted version for now
     # (since the current email message is only relevant for hosted users)
-    if System.get_env("BACKEND_URL") == "app.papercups.io" do
+    # TODO: we should also probably come up with a less generic environment
+    # variable name than "DOMAIN"... maybe "MAILGUN_DOMAIN"?
+    if System.get_env("DOMAIN") == "mail.papercups.io" do
       Logger.info("Sending welcome email: #{email}")
       ChatApi.Emails.send_welcome_email(email)
     end
