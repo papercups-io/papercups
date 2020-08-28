6bdbc9c9680230667ec1503bbd2c32677cb7ad88
reichert621 <reichertjalex@gmail.com>
2020-08-28 17:49:46 -0400

    Hide billing tab if stripe key is not valid

--- assets/src/components/Dashboard.tsx
+++ assets/src/components/Dashboard.tsx
@@ -34,12 +34,19 @@ import IntegrationsOverview from './integrations/IntegrationsOverview';
 import BillingOverview from './billing/BillingOverview';
 import CustomersPage from './customers/CustomersPage';
 
+const hasValidStripeKey = () => {
+  const key = process.env.REACT_APP_STRIPE_PUBLIC_KEY;
+
+  return key && key.startsWith('pk_');
+};
+
 const Dashboard = (props: RouteComponentProps) => {
   const auth = useAuth();
   const {pathname} = useLocation();
   const {unreadByCategory: unread} = useConversations();
   const [section, key] = pathname.split('/').slice(1); // Slice off initial slash
   const totalNumUnread = (unread && unread.all) || 0;
+  const shouldDisplayBilling = hasValidStripeKey();
 
   const logout = () => auth.logout().then(() => props.history.push('/login'));
 
@@ -160,13 +167,15 @@ const Dashboard = (props: RouteComponentProps) => {
               >
                 <Link to="/integrations">Integrations</Link>
               </Menu.Item>
-              <Menu.Item
-                title="Billing"
-                icon={<CreditCardOutlined />}
-                key="billing"
-              >
-                <Link to="/billing">Billing</Link>
-              </Menu.Item>
+              {shouldDisplayBilling && (
+                <Menu.Item
+                  title="Billing"
+                  icon={<CreditCardOutlined />}
+                  key="billing"
+                >
+                  <Link to="/billing">Billing</Link>
+                </Menu.Item>
+              )}
             </Menu>
           </Box>
 
@@ -194,7 +203,6 @@ const Dashboard = (props: RouteComponentProps) => {
             component={GettingStartedOverview}
           />
           <Route path="/account*" component={AccountOverview} />
-          <Route path="/billing" component={BillingOverview} />
           <Route path="/customers" component={CustomersPage} />
           <Route path="/integrations/:type" component={IntegrationsOverview} />
           <Route path="/integrations" component={IntegrationsOverview} />
@@ -206,6 +214,9 @@ const Dashboard = (props: RouteComponentProps) => {
             component={PriorityConversations}
           />
           <Route path="/conversations/closed" component={ClosedConversations} />
+          {shouldDisplayBilling && (
+            <Route path="/billing" component={BillingOverview} />
+          )}
           <Route path="*" render={() => <Redirect to="/conversations/all" />} />
         </Switch>
       </Layout>
