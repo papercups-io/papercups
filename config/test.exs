6ed9b5f04fa0a6a15b747b9b18860d1b74c7df6b
Aaron Gunderson <aaron@skylla.tech>
2020-08-27 21:08:23 -0400

    Allow test database variables to be overridden

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :chat_api, ChatApi.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_USER") || "postgres",
+  password: System.get_env("DATABASE_PASSWORD") || "postgres",
   database: "chat_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
