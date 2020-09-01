6c26ae1783f64e3c4cb417c1511ebf41a2e4fb2b
reichert621 <reichertjalex@gmail.com>
2020-09-01 14:40:15 -0400

    Set up two potential versions of pricing UI

--- assets/src/components/billing/PricingOverview.tsx
+++ assets/src/components/billing/PricingOverview.tsx
@@ -1,9 +1,85 @@
 import React from 'react';
 import {Box, Flex} from 'theme-ui';
-import {Button, Paragraph, Text, Title} from '../common';
+import {Button, Divider, Paragraph, Text, Title} from '../common';
 
 type Props = {};
 type State = {};
+
+const PricingCard = ({
+  title,
+  description,
+  cta,
+  pricing,
+  features,
+}: {
+  title: string;
+  description: string;
+  cta: React.ReactElement;
+  pricing: React.ReactElement;
+  features: React.ReactElement;
+}) => {
+  return (
+    <Box
+      mx={2}
+      p={3}
+      sx={{
+        flex: 1,
+        border: '1px solid #f5f5f5',
+        borderRadius: 4,
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 8px',
+      }}
+    >
+      <Title level={3}>{title}</Title>
+      <Paragraph style={{minHeight: 44}}>{description}</Paragraph>
+
+      <Box my={3}>{cta}</Box>
+
+      <Box sx={{fontSize: 16}}>{pricing}</Box>
+
+      <Divider />
+
+      {features}
+    </Box>
+  );
+};
+
+const PricingSection = ({
+  title,
+  description,
+  cta,
+  pricing,
+  features,
+  bordered,
+}: {
+  title: string;
+  description: string;
+  cta: React.ReactElement;
+  pricing: React.ReactElement;
+  features: React.ReactElement;
+  bordered?: boolean;
+}) => {
+  return (
+    <Box
+      mx={2}
+      p={3}
+      sx={{
+        flex: 1,
+        border: bordered ? '1px solid #f5f5f5' : 'none',
+      }}
+    >
+      <Title level={3}>{title}</Title>
+      <Paragraph style={{minHeight: 44}}>{description}</Paragraph>
+
+      <Box my={3}>{cta}</Box>
+
+      <Box sx={{fontSize: 16}}>{pricing}</Box>
+
+      <Divider />
+
+      {features}
+    </Box>
+  );
+};
 
 class PricingOverview extends React.Component<Props, State> {
   state = {};
@@ -16,96 +92,174 @@ class PricingOverview extends React.Component<Props, State> {
     return (
       <Box p={4}>
         <Box mb={4}>
-          <Title level={2}>Pricing Overview</Title>
-          <Paragraph>
-            <Text>Pricing details will go here!</Text>
-          </Paragraph>
+          <Title level={1}>Pricing Overview</Title>
         </Box>
 
         <Flex mx={-2} sx={{maxWidth: 960}}>
-          <Box
-            mx={2}
-            p={3}
-            sx={{
-              flex: 1,
-              border: '1px solid #f5f5f5',
-              borderRadius: 4,
-              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 8px',
-            }}
-          >
-            <Title level={3}>Starter</Title>
-            <Paragraph>Basic live chat and inbox to get started</Paragraph>
-            <Box>$0</Box>
-            <Box>forever</Box>
-
-            <Box my={2}>
-              <Button type="primary" size="large" block>
+          <PricingCard
+            title="Starter"
+            description="Basic live chat and inbox to get you started."
+            cta={
+              <Button type="primary" size="large" block ghost>
                 Create free account
               </Button>
-            </Box>
+            }
+            pricing={
+              <Text>
+                <Text strong>$0</Text> forever
+              </Text>
+            }
+            features={
+              <>
+                <Paragraph>Comes with:</Paragraph>
 
-            <Box>2 agents</Box>
-            <Box>Unlimited conversations</Box>
-            <Box>Customizable chat widget</Box>
-            <Box>Slack integration</Box>
-          </Box>
+                <Paragraph>
+                  <li>2 agents</li>
+                  <li>Unlimited conversations</li>
+                  <li>Customizable chat widget</li>
+                  <li>Slack integration</li>
+                </Paragraph>
+              </>
+            }
+          />
 
-          <Box
-            mx={2}
-            p={3}
-            sx={{
-              flex: 1,
-              border: '1px solid #f5f5f5',
-              borderRadius: 4,
-              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 8px',
-            }}
-          >
-            <Title level={3}>Team</Title>
-            <Paragraph>Supercharge your customer messaging</Paragraph>
-
-            <Box>$40</Box>
-            <Box>per month</Box>
-
-            <Box my={2}>
+          <PricingCard
+            title="Team"
+            description="Supercharge your support, sales, and marketing."
+            cta={
               <Button type="primary" size="large" block>
                 Create an account
               </Button>
-            </Box>
+            }
+            pricing={
+              <Text>
+                <Text strong>$40</Text>/month
+              </Text>
+            }
+            features={
+              <>
+                <Paragraph>
+                  Everything in <Text strong>Starter</Text> plus:
+                </Paragraph>
 
-            <Box>5 agents</Box>
-            <Box>Everything in Community plus:</Box>
-            <Box>Chat bots</Box>
-            <Box>Additional integrations</Box>
-            <Box>Webhooks</Box>
-          </Box>
+                <Paragraph>
+                  <li>Includes 5 seats</li>
+                  <li>Chat bots</li>
+                  <li>Additional integrations</li>
+                  <li>Webhooks</li>
+                </Paragraph>
+              </>
+            }
+          />
 
-          <Box
-            mx={2}
-            p={3}
-            sx={{
-              flex: 1,
-              border: '1px solid #f5f5f5',
-              borderRadius: 4,
-              boxShadow: 'rgba(0, 0, 0, 0.1) 0px 0px 8px',
-            }}
-          >
-            <Title level={3}>Custom</Title>
-            <Paragraph>Advanced workflows and support</Paragraph>
-
-            <Box>Custom</Box>
-            <Box>contact us</Box>
-
-            <Box my={2}>
-              <Button type="primary" size="large" block>
+          <PricingCard
+            title="Custom"
+            description="Advanced workflows, security, and support."
+            cta={
+              <Button type="primary" size="large" block ghost>
                 Chat with us
               </Button>
-            </Box>
+            }
+            pricing={<Text>Custom pricing</Text>}
+            features={
+              <>
+                <Paragraph>
+                  Everything in <Text strong>Team</Text> plus:
+                </Paragraph>
 
-            <Box>Unlimited agents</Box>
-            <Box>Everything in Team plus:</Box>
-            <Box>First-class support</Box>
-            <Box>Custom integrations</Box>
-          </Box>
+                <Paragraph>
+                  <li>Unlimited agents</li>
+                  <li>First-class support</li>
+                  <li>Custom integrations</li>
+                </Paragraph>
+              </>
+            }
+          />
+        </Flex>
+
+        <Divider />
+
+        <Flex mx={-2} sx={{maxWidth: 960}}>
+          <PricingSection
+            title="Starter"
+            description="Basic live chat and inbox to get you started."
+            cta={
+              <Button type="primary" size="large" block ghost>
+                Create free account
+              </Button>
+            }
+            pricing={
+              <Text>
+                <Text strong>$0</Text> forever
+              </Text>
+            }
+            features={
+              <>
+                <Paragraph>Comes with:</Paragraph>
+
+                <Paragraph>
+                  <li>2 agents</li>
+                  <li>Unlimited conversations</li>
+                  <li>Customizable chat widget</li>
+                  <li>Slack integration</li>
+                </Paragraph>
+              </>
+            }
+          />
+
+          <PricingSection
+            title="Team"
+            description="Supercharge your support, sales, and marketing."
+            cta={
+              <Button type="primary" size="large" block>
+                Create an account
+              </Button>
+            }
+            pricing={
+              <Text>
+                <Text strong>$40</Text>/month
+              </Text>
+            }
+            features={
+              <>
+                <Paragraph>
+                  Everything in <Text strong>Starter</Text> plus:
+                </Paragraph>
+
+                <Paragraph>
+                  <li>Includes 5 seats</li>
+                  <li>Chat bots</li>
+                  <li>Additional integrations</li>
+                  <li>Webhooks</li>
+                </Paragraph>
+              </>
+            }
+            bordered
+          />
+
+          <PricingSection
+            title="Custom"
+            description="Advanced workflows, security, and support."
+            cta={
+              <Button type="primary" size="large" block ghost>
+                Chat with us
+              </Button>
+            }
+            pricing={<Text>Custom pricing</Text>}
+            features={
+              <>
+                <Paragraph>
+                  Everything in <Text strong>Team</Text> plus:
+                </Paragraph>
+
+                <Paragraph>
+                  <li>Unlimited agents</li>
+                  <li>First-class support</li>
+                  <li>Custom integrations</li>
+                </Paragraph>
+              </>
+            }
+          />
         </Flex>
       </Box>
     );
