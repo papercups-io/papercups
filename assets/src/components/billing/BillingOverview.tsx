e2a6af7e1fbeee2d0ff6a4bd8167da14ac1fcf83
Alex Reichert <reichertjalex@gmail.com>
2020-09-01 23:21:07 -0400

    Minor improvements to billing overview page (#204)
    * Initial updates to billing overview page
    
    * More improvements

--- assets/src/components/billing/BillingOverview.tsx
+++ assets/src/components/billing/BillingOverview.tsx
@@ -1,16 +1,136 @@
 import React from 'react';
+import {capitalize} from 'lodash';
+import dayjs from 'dayjs';
 import {Box, Flex} from 'theme-ui';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import * as API from '../../api';
-import {colors, notification, Paragraph, Text, Title} from '../common';
-import {CreditCardTwoTone} from '../icons';
+import {
+  colors,
+  notification,
+  Button,
+  Modal,
+  Paragraph,
+  Table,
+  Text,
+  Title,
+} from '../common';
+import {CreditCardTwoTone, RightCircleOutlined} from '../icons';
 import Spinner from '../Spinner';
 import PaymentForm from './PaymentForm';
+import {PricingOptionsModal} from './PricingOverview';
+import './Billing.css';
 
 const stripe = loadStripe(
   process.env.REACT_APP_STRIPE_PUBLIC_KEY || 'pk_test_xxxxx'
 );
+
+enum Alignment {
+  Right = 'right',
+  Left = 'left',
+  Center = 'center',
+}
+
+type CreditCardBrand =
+  | 'amex'
+  | 'cartes_bancaires'
+  | 'diners_club'
+  | 'discover'
+  | 'jcb'
+  | 'mastercard'
+  | 'visa'
+  | 'unionpay'
+  | string;
+
+const getBrandName = (brand: CreditCardBrand) => {
+  switch (brand) {
+    case 'visa':
+      return 'Visa';
+    case 'mastercard':
+      return 'MasterCard';
+    case 'amex':
+      return 'American Express';
+    case 'discover':
+      return 'Discover';
+    case 'unionpay':
+      return 'UnionPay';
+    case 'diners_club':
+      return 'Diners Club';
+    case 'cartes_bancaires':
+      return 'Cartes Bancaires';
+    case 'jcb':
+      return 'JCB';
+    default:
+      return brand
+        .split('_')
+        .map((str) => capitalize(str))
+        .join(' ');
+  }
+};
+
+const BillingBreakdownTable = () => {
+  const columns = [
+    {
+      title: 'Subscription',
+      dataIndex: 'feature',
+      key: 'feature',
+      render: (value: string, record: any) => {
+        const {includes = []} = record;
+
+        return (
+          <Box>
+            <Box mb={includes.length ? 2 : 0}>
+              <Text strong>{value}</Text>
+            </Box>
+            {includes.map((included: any) => {
+              const {feature} = included;
+
+              return (
+                <Flex key={feature} mb={2} sx={{alignItems: 'center'}}>
+                  <Box mr={2}>
+                    <RightCircleOutlined />
+                  </Box>
+                  <Text type="secondary">{feature}</Text>
+                </Flex>
+              );
+            })}
+          </Box>
+        );
+      },
+    },
+    {
+      title: 'Price per month',
+      dataIndex: 'price',
+      key: 'price',
+      align: Alignment.Right,
+      className: 'BillingBreakdownTable-priceCell',
+      render: (price: number) => {
+        return (
+          <Text strong>
+            {price < 0 ? '-' : ''}${Math.abs(price).toFixed(2)}
+          </Text>
+        );
+      },
+    },
+  ];
+  // Just hard-coding for now
+  const data = [
+    {
+      key: 'plan',
+      feature: 'Starter plan',
+      includes: [
+        {feature: '2 seats'},
+        {feature: '100,000 messages'},
+        {feature: 'Slack integration'},
+      ],
+      price: 0,
+    },
+    {key: 'discount', feature: 'Discount', price: 0},
+    {key: 'total', feature: 'Total', price: 0},
+  ];
+
+  return <Table dataSource={data} columns={columns} pagination={false} />;
+};
 
 type Props = {};
 type State = {
@@ -41,28 +161,24 @@ class BillingOverview extends React.Component<Props, State> {
   formatPaymentMethodInfo = (paymentMethod?: any) => {
     if (!paymentMethod) {
       return (
-        <Flex sx={{alignItems: 'center', my: 1}}>
-          <Text type="secondary">None</Text>
-        </Flex>
+        <Text type="secondary">
+          No credit card information has been entered yet.
+        </Text>
       );
     }
 
-    const {last4, exp_month, exp_year} = paymentMethod;
+    const {brand, last4, exp_month, exp_year} = paymentMethod;
+    const brandName = getBrandName(brand);
+    const expiresAt = dayjs()
+      .set('month', exp_month - 1)
+      .set('year', exp_year)
+      .format('MMM YYYY');
 
     return (
-      <Flex sx={{alignItems: 'center', my: 1}}>
-        <Box mr={2}>
-          <CreditCardTwoTone twoToneColor={colors.primary} />
-        </Box>
-        <Text keyboard style={{letterSpacing: 0.6}}>
-          ••••••••••••{last4}
-        </Text>
-        <Box ml={2}>
-          <Text type="secondary">
-            expires {exp_month}/{exp_year}
-          </Text>
-        </Box>
-      </Flex>
+      <Text type="secondary">
+        Billed to <CreditCardTwoTone twoToneColor={colors.primary} />{' '}
+        {brandName} ending in {last4} (expires {expiresAt})
+      </Text>
     );
   };
 
@@ -96,12 +212,30 @@ class BillingOverview extends React.Component<Props, State> {
               </span>
             </Text>
           </Paragraph>
+
+          {/* TODO: implement logic to select subscription plan */}
+          {false && (
+            <Box>
+              <Button>Select plan</Button>
+
+              <Modal
+                title="Select plan"
+                visible={false}
+                width={800}
+                onOk={console.log}
+                onCancel={console.log}
+              >
+                <PricingOptionsModal />
+              </Modal>
+            </Box>
+          )}
         </Box>
 
         <Box mb={4}>
-          <Text strong>Default payment method</Text>
-          <Box>{this.formatPaymentMethodInfo(defaultPaymentMethod)}</Box>
+          <BillingBreakdownTable />
         </Box>
+
+        <Box mb={4}>{this.formatPaymentMethodInfo(defaultPaymentMethod)}</Box>
 
         <Box mb={4}>
           <Elements stripe={stripe}>
