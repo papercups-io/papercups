d393e9ddfe098458fa29d557ba8e88bc2556663e
Alex Reichert <reichertjalex@gmail.com>
2020-09-18 17:24:56 -0400

    Add a bit more swagger docs for conversations and messages API endpoints (#237)

--- lib/chat_api_web/controllers/conversation_controller.ex
+++ lib/chat_api_web/controllers/conversation_controller.ex
@@ -7,10 +7,39 @@ defmodule ChatApiWeb.ConversationController do
 
   action_fallback(ChatApiWeb.FallbackController)
 
+  def swagger_definitions do
+    %{
+      Conversation:
+        swagger_schema do
+          title("Conversation")
+          description("A conversation in the app")
+
+          properties do
+            id(:string, "Conversation ID")
+            status(:string, "Conversation status", required: true)
+            priority(:string, "Priority status of the conversation")
+            account_id(:string, "The ID of the associated account", required: true)
+            customer_id(:string, "The ID of the customer", required: true)
+            assignee_id(:string, "The ID of the assigned user")
+            created_at(:string, "Created timestamp", format: :datetime)
+            updated_at(:string, "Updated timestamp", format: :datetime)
+          end
+
+          example(%{
+            status: "open",
+            priority: "not_priority",
+            account_id: "acct_1a2b3c",
+            customer_id: "cus_1a2b3c"
+          })
+        end
+    }
+  end
+
   swagger_path :index do
     get("/api/conversations")
     summary("Query for conversations")
     description("Query for conversations. This operation supports filtering")
+
     parameter("Authorization", :header, :string, "OAuth2 access token", required: true)
 
     parameters do
@@ -26,9 +55,10 @@ defmodule ChatApiWeb.ConversationController do
     end
 
     response(200, "Success")
+    response(401, "Not authenticated")
   end
 
-  @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, params) do
     with %{account_id: account_id} <- conn.assigns.current_user do
       conversations = Conversations.list_conversations_by_account(account_id, params)
@@ -37,12 +67,25 @@ defmodule ChatApiWeb.ConversationController do
     end
   end
 
+  @spec find_by_customer(Plug.Conn.t(), map) :: Plug.Conn.t()
   def find_by_customer(conn, %{"customer_id" => customer_id, "account_id" => account_id}) do
     conversations = Conversations.find_by_customer(customer_id, account_id)
 
     render(conn, "index.json", conversations: conversations)
   end
 
+  swagger_path :create do
+    post("/api/conversations")
+    summary("Create a conversation")
+    description("Create a new conversation")
+
+    parameter(:conversation, :body, :object, "The conversation details")
+
+    response(201, "Success")
+    response(422, "Unprocessable entity")
+  end
+
+  @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def create(conn, %{"conversation" => conversation_params}) do
     with {:ok, %Conversation{} = conversation} <-
            Conversations.create_conversation(conversation_params) do
@@ -59,11 +102,38 @@ defmodule ChatApiWeb.ConversationController do
     end
   end
 
+  swagger_path :show do
+    get("/api/conversations/{id}")
+    summary("Retrieve a conversation")
+    description("Retrieve an existing conversation")
+
+    parameter("Authorization", :header, :string, "OAuth2 access token", required: true)
+    parameter(:id, :path, :string, "Conversation ID", required: true)
+
+    response(200, "Success")
+    response(401, "Not authenticated")
+  end
+
+  @spec show(Plug.Conn.t(), map) :: Plug.Conn.t()
   def show(conn, %{"id" => id}) do
     conversation = Conversations.get_conversation!(id)
     render(conn, "show.json", conversation: conversation)
   end
 
+  swagger_path :update do
+    put("/api/conversations/{id}")
+    summary("Update a conversation")
+    description("Update an existing conversation")
+
+    parameter("Authorization", :header, :string, "OAuth2 access token", required: true)
+    parameter(:id, :path, :string, "Conversation ID", required: true)
+    parameter(:conversation, :body, :object, "The conversation updates")
+
+    response(200, "Success")
+    response(401, "Not authenticated")
+  end
+
+  @spec update(Plug.Conn.t(), map) :: Plug.Conn.t()
   def update(conn, %{"id" => id, "conversation" => conversation_params}) do
     conversation = Conversations.get_conversation!(id)
 
@@ -73,6 +143,7 @@ defmodule ChatApiWeb.ConversationController do
     end
   end
 
+  @spec delete(Plug.Conn.t(), map) :: Plug.Conn.t()
   def delete(conn, %{"id" => id}) do
     conversation = Conversations.get_conversation!(id)
 
--- lib/chat_api_web/controllers/message_controller.ex
+++ lib/chat_api_web/controllers/message_controller.ex
@@ -1,11 +1,53 @@
 defmodule ChatApiWeb.MessageController do
   use ChatApiWeb, :controller
+  use PhoenixSwagger
 
   alias ChatApi.Messages
   alias ChatApi.Messages.Message
 
   action_fallback(ChatApiWeb.FallbackController)
 
+  def swagger_definitions do
+    %{
+      Message:
+        swagger_schema do
+          title("Message")
+          description("A message in the app")
+
+          properties do
+            id(:string, "Message ID")
+            body(:string, "Message body", required: true)
+            account_id(:string, "The ID of the associated account", required: true)
+            conversation_id(:string, "The ID of the associated conversation", required: true)
+            customer_id(:string, "The ID of the customer")
+            user_id(:string, "The ID of the user/agent")
+            created_at(:string, "Created timestamp", format: :datetime)
+            updated_at(:string, "Updated timestamp", format: :datetime)
+          end
+
+          example(%{
+            body: "Hello world!",
+            customer_id: "cus_1a2b3c",
+            conversation_id: "conv_1a2b3c",
+            account_id: "acct_1a2b3c",
+            user_id: "user_1a2b3c"
+          })
+        end
+    }
+  end
+
+  swagger_path :index do
+    get("/api/messages")
+    summary("Query for messages")
+    description("Query for messages.")
+
+    parameter("Authorization", :header, :string, "OAuth2 access token", required: true)
+
+    response(200, "Success")
+    response(401, "Not authenticated")
+  end
+
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     with %{account_id: account_id} <- conn.assigns.current_user do
       messages = Messages.list_messages(account_id)
@@ -13,6 +55,7 @@ defmodule ChatApiWeb.MessageController do
     end
   end
 
+  @spec count(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def count(conn, _params) do
     with %{account_id: account_id} <- conn.assigns.current_user do
       count = Messages.count_messages_by_account(account_id)
@@ -21,6 +64,20 @@ defmodule ChatApiWeb.MessageController do
     end
   end
 
+  swagger_path :create do
+    post("/api/messages")
+    summary("Create a message")
+    description("Create a new message")
+
+    parameter("Authorization", :header, :string, "OAuth2 access token", required: true)
+    parameter(:message, :body, :object, "The message details")
+
+    response(201, "Success")
+    response(422, "Unprocessable entity")
+    response(401, "Not authenticated")
+  end
+
+  @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def create(conn, %{"message" => message_params}) do
     with %{id: user_id, account_id: account_id} <- conn.assigns.current_user,
          {:ok, %Message{} = msg} <-
@@ -38,11 +95,25 @@ defmodule ChatApiWeb.MessageController do
     end
   end
 
+  swagger_path :show do
+    get("/api/messages/{id}")
+    summary("Retrieve a message")
+    description("Retrieve an existing message")
+
+    parameter("Authorization", :header, :string, "OAuth2 access token", required: true)
+    parameter(:id, :path, :string, "Message ID", required: true)
+
+    response(200, "Success")
+    response(401, "Not authenticated")
+  end
+
+  @spec show(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def show(conn, %{"id" => id}) do
     message = Messages.get_message!(id)
     render(conn, "show.json", message: message)
   end
 
+  @spec update(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def update(conn, %{"id" => id, "message" => message_params}) do
     message = Messages.get_message!(id)
 
@@ -51,6 +122,7 @@ defmodule ChatApiWeb.MessageController do
     end
   end
 
+  @spec delete(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def delete(conn, %{"id" => id}) do
     message = Messages.get_message!(id)
 
