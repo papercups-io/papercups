1a8f186d635133caf7d526de58b64f0fcb22adef
reichert621 <reichertjalex@gmail.com>
2020-09-23 19:30:05 -0400

    Add send_message method to gmail module

--- lib/chat_api/google/gmail.ex
+++ lib/chat_api/google/gmail.ex
@@ -1,4 +1,17 @@
 defmodule ChatApi.Google.Gmail do
+  def send_message(refresh_token, to: to, from: from, subject: subject, message: message) do
+    with %{token: %{access_token: access_token}} <-
+           ChatApi.Google.Auth.get_token!(refresh_token: refresh_token) do
+      ChatApi.Emails.send_via_gmail(
+        to: to,
+        from: from,
+        subject: subject,
+        message: message,
+        access_token: access_token
+      )
+    end
+  end
+
   def list_messages(refresh_token) do
     scope = "https://gmail.googleapis.com/gmail/v1/users/me/messages"
     client = ChatApi.Google.Auth.get_token!(refresh_token: refresh_token)
