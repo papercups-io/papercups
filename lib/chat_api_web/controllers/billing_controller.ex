41cf0d7eefcd440edeed611192bca91e7868ad2f
Jamie Neubert Pedersen <JamieNeubert@gmail.com>
2020-10-07 21:54:17 +0200

    Add slack notification on plan changes

--- lib/chat_api_web/controllers/billing_controller.ex
+++ lib/chat_api_web/controllers/billing_controller.ex
@@ -17,6 +17,7 @@ defmodule ChatApiWeb.BillingController do
     with %{account_id: account_id} <- conn.assigns.current_user,
          account <- Accounts.get_account!(account_id),
          {:ok, _account} <- Billing.create_subscription_plan(account, plan) do
+      notify_slack(conn)
       json(conn, %{data: %{ok: true}})
     end
   end
@@ -25,7 +26,21 @@ defmodule ChatApiWeb.BillingController do
     with %{account_id: account_id} <- conn.assigns.current_user,
          account <- Accounts.get_account!(account_id),
          {:ok, _account} <- Billing.update_subscription_plan(account, plan) do
+      notify_slack(conn)
       json(conn, %{data: %{ok: true}})
     end
+  end
+
+  @spec notify_slack(Conn.t()) :: Conn.t()
+  defp notify_slack(conn) do
+    with %{email: email, plan: plan} <- conn.assigns.current_user do
+      # Putting in an async Task for now, since we don't care if this succeeds
+      # or fails (and we also don't want it to block anything)
+      Task.start(fn ->
+        ChatApi.Slack.log("#{email} set subscription plan to #{plan}")
+      end)
+    end
+
+    conn
   end
 end
