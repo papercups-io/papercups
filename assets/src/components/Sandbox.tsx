bb4de2605b4a5ca85129519de1d1cf738ec0b2dc
reichert621 <reichertjalex@gmail.com>
2020-10-07 15:57:38 -0400

    Add comment about sandbox page

--- assets/src/components/Sandbox.tsx
+++ assets/src/components/Sandbox.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Flex} from 'theme-ui';
 
 export const Sandbox = () => {
+  // Just using this page as a place to hack on UI components before they're ready
   return (
     <Flex p={2} sx={{flex: 1}}>
       UI Sandbox!
