7a2d4c40cfb52a5d2eb14cb144c0840e7218e824
Daniel Khaapamyaki <dasky@hey.com>
2020-10-27 00:09:53 +0100

    add migaration

---
+++ priv/repo/migrations/20201026201955_add_archived_at_field_to_conversations.exs
@@ -0,0 +1,9 @@
+defmodule ChatApi.Repo.Migrations.AddArchivedAtFieldToConversations do
+  use Ecto.Migration
+
+  def change do
+    alter table(:conversations) do
+      add :archived_at, :utc_datetime
+    end
+  end
+end
