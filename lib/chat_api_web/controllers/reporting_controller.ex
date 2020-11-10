401ce27d4184279a65bfb78a69e1e3a9aa19e735
reichert621 <reichertjalex@gmail.com>
2020-11-10 17:54:32 -0500

    Add customer breakdown by browser/os/timezone to reporting API response

--- lib/chat_api_web/controllers/reporting_controller.ex
+++ lib/chat_api_web/controllers/reporting_controller.ex
@@ -22,7 +22,12 @@ defmodule ChatApiWeb.ReportingController do
         messages_per_user: Reporting.count_messages_per_user(account_id, filters),
         messages_by_weekday: Reporting.count_messages_by_weekday(account_id, filters),
         sent_messages_by_date: Reporting.count_sent_messages_by_date(account_id, filters),
-        received_messages_by_date: Reporting.count_received_messages_by_date(account_id, filters)
+        received_messages_by_date: Reporting.count_received_messages_by_date(account_id, filters),
+        customer_breakdown_by_browser:
+          Reporting.get_customer_breakdown(account_id, :browser, filters),
+        customer_breakdown_by_os: Reporting.get_customer_breakdown(account_id, :os, filters),
+        customer_breakdown_by_time_zone:
+          Reporting.get_customer_breakdown(account_id, :time_zone, filters)
       }
     })
   end
