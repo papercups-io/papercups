95184b8dea4c1c64a3b1135a9161e86b94f0e838
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2020-12-07 22:29:21 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule ChatApi.MixProject do
       {:phoenix_swagger, "~> 0.8"},
       {:ex_json_schema, "~> 0.5"},
       {:ex_machina, "~> 2.4", only: [:test]},
-      {:pow_postgres_store, "~> 1.0.0-rc2"}
+      {:pow_postgres_store, "~> 1.0.0-rc2"},
       {:tzdata, "~> 1.0.5"}
     ]
   end
