045e7bf3a195e949f765c3834f7058939408f551
reichert621 <reichertjalex@gmail.com>
2020-12-07 14:28:04 -0500

    Add timezone database, make it easier to work with timezones on the server

--- lib/chat_api/accounts/working_hours.ex
+++ lib/chat_api/accounts/working_hours.ex
@@ -1,6 +1,20 @@
 defmodule ChatApi.Accounts.WorkingHours do
   use Ecto.Schema
   import Ecto.Changeset
+  alias ChatApi.Accounts.WorkingHours
+
+  @day_to_indexes %{
+    "everyday" => [1, 2, 3, 4, 5, 6, 7],
+    "weekdays" => [1, 2, 3, 4, 5],
+    "weekends" => [6, 7],
+    "monday" => [1],
+    "tuesday" => [2],
+    "wednesday" => [3],
+    "thursday" => [4],
+    "friday" => [5],
+    "saturday" => [6],
+    "sunday" => [7]
+  }
 
   embedded_schema do
     field(:day, :string)
@@ -11,5 +25,10 @@ defmodule ChatApi.Accounts.WorkingHours do
   def changeset(schema, params) do
     schema
     |> cast(params, [:day, :start_minute, :end_minute])
+  end
+
+  @spec day_to_indexes(map()) :: [Calendar.day()]
+  def day_to_indexes(%WorkingHours{day: day}) do
+    Map.fetch!(@day_to_indexes, day)
   end
 end
--- mix.exs
+++ mix.exs
@@ -66,7 +66,8 @@ defmodule ChatApi.MixProject do
       {:mail, "~> 0.2"},
       {:phoenix_swagger, "~> 0.8"},
       {:ex_json_schema, "~> 0.5"},
-      {:pow_postgres_store, "~> 1.0.0-rc2"}
+      {:pow_postgres_store, "~> 1.0.0-rc2"},
+      {:tzdata, "~> 1.0.5"}
     ]
   end
 
