43b52f3516a98a7d41468e2e97bffd6d453e7707
Frank Kotsianas <frankkotsianas@gmail.com>
2020-12-11 03:33:57 -0500

    Notes tests pass

--- test/chat_api/notes_test.exs
+++ test/chat_api/notes_test.exs
@@ -1,77 +1,70 @@
 defmodule ChatApi.NotesTest do
   use ChatApi.DataCase
 
+  import ChatApi.Factory
   alias ChatApi.Notes
 
   describe "notes" do
     alias ChatApi.Notes.Note
 
-    @user_attrs %{
-      display_name: "some display_name",
-      full_name: "some full_name",
-      profile_photo_url: "some profile_photo_url"
-    }
-
-    @valid_attrs %{body: "some body", author_id: 1, conver}
     @update_attrs %{body: "some updated body"}
     @invalid_attrs %{body: nil}
 
     setup do
       account = insert(:account)
+      agent = insert(:user, account: account)
       customer = insert(:customer, account: account)
-      user = insert(:user)}
+      note = insert(:note, body: "note_body", account: account, author: agent, customer: customer)
 
-      {:ok, user: user, account: account, customer: customer}
+      {:ok,
+        agent: agent,
+        account: account,
+        customer: customer,
+        note: note,
+      }
     end
 
-    def note_fixture(attrs \\ %{}) do
-      {:ok, note, user, customer} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Notes.create_note()
-
-      note
+    test "list_notes/1 returns all notes for the given account and customer", %{note: note, account: account, customer: customer} do
+      note_ids = Notes.list_notes_for_customer(%{account_id: account.id, customer_id: customer.id}) |> Enum.map(& &1.id)
+      assert note_ids == [note.id]
     end
 
-    test "list_notes/0 returns all notes" do
-      note = note_fixture()
-      assert Notes.list_notes() == [note]
+    test "get_note!/1 returns the note with given id", %{note: note} do
+      found = Notes.get_note!(note.id)
+      assert found.id == note.id
+      assert found.body == note.body
     end
 
-    test "get_note!/1 returns the note with given id" do
-      note = note_fixture()
-      assert Notes.get_note!(note.id) == note
-    end
-
-    test "create_note/1 with valid data creates a note" do
-      assert {:ok, %Note{} = note} = Notes.create_note(@valid_attrs)
+    test "create_note/1 with valid data creates a note", %{account: account, customer: customer, agent: agent} do
+      assert {:ok, %Note{} = note} = Notes.create_note(%{body: "some body", account_id: account.id, customer_id: customer.id, author_id: agent.id})
       assert note.body == "some body"
+      assert note.customer_id == customer.id
+      assert note.author_id == agent.id
+      assert note.account_id == account.id
     end
 
     test "create_note/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Notes.create_note(@invalid_attrs)
     end
 
-    test "update_note/2 with valid data updates the note" do
-      note = note_fixture()
+    test "update_note/2 with valid data updates the note", %{note: note} do
       assert {:ok, %Note{} = note} = Notes.update_note(note, @update_attrs)
       assert note.body == "some updated body"
     end
 
-    test "update_note/2 with invalid data returns error changeset" do
-      note = note_fixture()
+    test "update_note/2 with invalid data returns error changeset", %{note: note} do
       assert {:error, %Ecto.Changeset{}} = Notes.update_note(note, @invalid_attrs)
-      assert note == Notes.get_note!(note.id)
+      found = Notes.get_note!(note.id)
+      assert found.id == note.id
+      assert found.updated_at == note.updated_at
     end
 
-    test "delete_note/1 deletes the note" do
-      note = note_fixture()
+    test "delete_note/1 deletes the note", %{note: note} do
       assert {:ok, %Note{}} = Notes.delete_note(note)
       assert_raise Ecto.NoResultsError, fn -> Notes.get_note!(note.id) end
     end
 
-    test "change_note/1 returns a note changeset" do
-      note = note_fixture()
+    test "change_note/1 returns a note changeset", %{note: note} do
       assert %Ecto.Changeset{} = Notes.change_note(note)
     end
   end
