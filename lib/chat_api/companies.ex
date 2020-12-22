aef12b279989246df3fd6545b460e59b3939df9e
reichert621 <reichertjalex@gmail.com>
2020-12-21 21:29:45 -0800

    Add specs to Companies context

--- lib/chat_api/companies.ex
+++ lib/chat_api/companies.ex
@@ -13,83 +13,29 @@ defmodule ChatApi.Companies do
     Company |> where(account_id: ^account_id) |> Repo.all()
   end
 
-  @doc """
-  Gets a single company.
-
-  Raises `Ecto.NoResultsError` if the Company does not exist.
-
-  ## Examples
-
-      iex> get_company!(123)
-      %Company{}
-
-      iex> get_company!(456)
-      ** (Ecto.NoResultsError)
-
-  """
+  @spec get_company!(binary()) :: Company.t()
   def get_company!(id), do: Repo.get!(Company, id)
 
-  @doc """
-  Creates a company.
-
-  ## Examples
-
-      iex> create_company(%{field: value})
-      {:ok, %Company{}}
-
-      iex> create_company(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
+  @spec create_company(map()) :: {:ok, Company.t()} | {:error, Ecto.Changeset.t()}
   def create_company(attrs \\ %{}) do
     %Company{}
     |> Company.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a company.
-
-  ## Examples
-
-      iex> update_company(company, %{field: new_value})
-      {:ok, %Company{}}
-
-      iex> update_company(company, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
+  @spec update_company(Company.t(), map()) :: {:ok, Company.t()} | {:error, Ecto.Changeset.t()}
   def update_company(%Company{} = company, attrs) do
     company
     |> Company.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a company.
-
-  ## Examples
-
-      iex> delete_company(company)
-      {:ok, %Company{}}
-
-      iex> delete_company(company)
-      {:error, %Ecto.Changeset{}}
-
-  """
+  @spec delete_company(Company.t()) :: {:ok, Company.t()} | {:error, Ecto.Changeset.t()}
   def delete_company(%Company{} = company) do
     Repo.delete(company)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking company changes.
-
-  ## Examples
-
-      iex> change_company(company)
-      %Ecto.Changeset{data: %Company{}}
-
-  """
+  @spec change_company(Company.t(), map()) :: Ecto.Changeset.t()
   def change_company(%Company{} = company, attrs \\ %{}) do
     Company.changeset(company, attrs)
   end
