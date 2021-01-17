1129fe4b4a1bab4027d1c8b252ca48a0fbe3d0ca
cheeseblubber <kamleung911@gmail.com>
2021-01-17 14:15:21 -0500

    fix merge

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule ChatApi.MixProject do
       {:oauth2, "~> 0.9"},
       {:mail, "~> 0.2"},
       {:phoenix_swagger, "~> 0.8"},
-      {:uuid, "~> 1.1"}
+      {:uuid, "~> 1.1"},
       {:ex_json_schema, "~> 0.5"},
       {:pow_postgres_store, "~> 1.0.0-rc2"},
       {:tzdata, "~> 1.0.5"},
