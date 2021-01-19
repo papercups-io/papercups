09779a46054e61bb29bb24ba9a99efc2cdf4206d
reichert621 <reichertjalex@gmail.com>
2021-01-19 15:47:59 -0500

    Play around with gmail api (currently unused)

--- lib/chat_api/google/gmail.ex
+++ lib/chat_api/google/gmail.ex
@@ -47,13 +47,13 @@ defmodule ChatApi.Google.Gmail do
   end
 
   def decode_message_body(text) do
-    text |> String.replace("-", "+") |> Base.decode64()
+    text |> String.replace("-", "+") |> String.replace("_", "/") |> Base.decode64()
   end
 
   # Example use case:
-  #
-  # ChatApi.Google.Gmail.list_threads(token, in: "sent", subject: "October product updates")
-  #   |> Map.get("threads")
+  #  token
+  #   |> ChatApi.Google.Gmail.list_threads(in: "sent", subject: "October product updates")
+  #   |> Map.get("threads", [])
   #   |> Enum.map(fn thread ->
   #     thread
   #     |> Map.get("id")
@@ -66,9 +66,38 @@ defmodule ChatApi.Google.Gmail do
     thread
     |> Map.get("messages")
     |> List.first()
-    |> Map.get("payload")
-    |> Map.get("headers")
+    |> get_in(["payload", "headers"])
     |> Enum.find(fn h -> h["name"] == "To" end)
     |> Map.get("value")
+  end
+
+  def get_thread_messages(thread) do
+    thread
+    |> Map.get("messages")
+    |> Enum.map(fn msg ->
+      snippet = Map.get(msg, "snippet")
+
+      msg
+      |> get_in(["payload", "parts"])
+      |> Enum.reduce(%{snippet: snippet}, fn part, acc ->
+        [key, value] =
+          case part do
+            %{"mimeType" => "text/plain", "body" => %{"data" => encoded}} ->
+              [:text, decode_message_body(encoded)]
+
+            %{"mimeType" => "text/html", "body" => %{"data" => encoded}} ->
+              [:html, decode_message_body(encoded)]
+
+            _ ->
+              [:invalid, nil]
+          end
+
+        case value do
+          {:ok, decoded} -> Map.merge(acc, %{key => decoded})
+          :error -> acc
+          _ -> acc
+        end
+      end)
+    end)
   end
 end
