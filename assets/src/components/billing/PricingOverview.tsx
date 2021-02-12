ff0359641ed72a196452d44258dd4f8b37b21696
cheeseblubber <kamleung911@gmail.com>
2021-02-12 13:36:00 -0500

    remove pricing dead code

--- assets/src/components/billing/PricingOverview.tsx
+++ assets/src/components/billing/PricingOverview.tsx
@@ -94,95 +94,6 @@ const PricingSection = ({
   );
 };
 
-export const PricingOptions = () => {
-  return (
-    <Flex mx={-2} sx={{maxWidth: 960}}>
-      <PricingCard
-        title="Starter"
-        description="Basic live chat and inbox to get you started."
-        cta={
-          <Button type="primary" size="large" block ghost>
-            Create free account
-          </Button>
-        }
-        pricing={
-          <Text>
-            <Text strong>$0</Text> forever
-          </Text>
-        }
-        features={
-          <>
-            <Paragraph>Comes with:</Paragraph>
-
-            <Paragraph>
-              <li>2 seats included</li>
-              <li>100,000 messages</li>
-              <li>30 day message retention</li>
-              <li>Customizable chat widget</li>
-            </Paragraph>
-          </>
-        }
-      />
-
-      <PricingCard
-        title="Team"
-        description="Supercharge your support, sales, and marketing."
-        cta={
-          <Button type="primary" size="large" block>
-            Create an account
-          </Button>
-        }
-        pricing={
-          <Text>
-            <Text strong>$94</Text>/month
-          </Text>
-        }
-        features={
-          <>
-            <Paragraph>
-              Everything in <Text strong>Starter</Text> plus:
-            </Paragraph>
-
-            <Paragraph>
-              <li>10 seats included</li>
-              <li>Reply from Slack</li>
-              <li>Unlimited data retention</li>
-              <li>Website screen sharing</li>
-              <li>Private notes</li>
-              <li>Webhooks</li>
-              <li>Priority support</li>
-            </Paragraph>
-          </>
-        }
-      />
-
-      <PricingCard
-        title="Enterprise"
-        description="Advanced workflows, security, and support."
-        cta={
-          <Button type="primary" size="large" block ghost>
-            Contact sales
-          </Button>
-        }
-        pricing={<Text>Custom pricing</Text>}
-        features={
-          <>
-            <Paragraph>
-              Everything in <Text strong>Team</Text> plus:
-            </Paragraph>
-
-            <Paragraph>
-              <li>Unlimited seats</li>
-              <li>On-premise deployment</li>
-              <li>Custom integrations</li>
-            </Paragraph>
-          </>
-        }
-      />
-    </Flex>
-  );
-};
-
 // TODO: move to separate file?
 export const PricingOptionsModal = ({
   pending,
@@ -266,10 +177,8 @@ export const PricingOptionsModal = ({
             <Paragraph>
               <ul>
                 <li>10 seats included</li>
-                <li>Reply from Slack</li>
                 <li>Unlimited data retention</li>
                 <li>Website screen sharing</li>
-                <li>Private notes</li>
                 <li>Webhooks</li>
                 <li>Priority support</li>
               </ul>
@@ -310,27 +219,3 @@ export const PricingOptionsModal = ({
     </Flex>
   );
 };
-
-class PricingOverview extends React.Component<Props, State> {
-  state = {};
-
-  async componentDidMount() {
-    //
-  }
-
-  render() {
-    return (
-      <Box p={4}>
-        <Box mb={4}>
-          <Title level={1}>Pricing Overview</Title>
-        </Box>
-
-        <PricingOptions />
-        <Divider />
-        <PricingOptionsModal selected={null} onSelectPlan={logger.debug} />
-      </Box>
-    );
-  }
-}
-
-export default PricingOverview;
