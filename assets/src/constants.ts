0cef55dc482ed031701e2de6a3e26c446e47b4c8
reichert621 <reichertjalex@gmail.com>
2021-03-12 15:06:10 -0500

    Update pricing

--- assets/src/constants.ts
+++ assets/src/constants.ts
@@ -1,5 +1,5 @@
 import {isEuEdition} from './config';
 
 export const STARTER_PRICE = isEuEdition ? 7 : 0;
-export const LITE_PRICE = 32;
-export const TEAM_PRICE = 94;
+export const LITE_PRICE = isEuEdition ? 39 : 32;
+export const TEAM_PRICE = isEuEdition ? 99 : 94;
