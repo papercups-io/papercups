f640ade17be7049308a858897f41e022f2eef2f6
Daven Casia <dubbinpaul@gmail.com>
2021-03-12 18:37:03 -0600

    Ran mix format

--- test/chat_api/messages_test.exs
+++ test/chat_api/messages_test.exs
@@ -38,7 +38,9 @@ defmodule ChatApi.MessagesTest do
       attrs = params_with_assocs(:message, body: "valid message body")
 
       assert {:ok, %Message{} = message} = Messages.create_message(attrs)
-      assert ChatApi.Conversations.get_conversation!(message.conversation_id).last_activity_at != nil
+
+      assert ChatApi.Conversations.get_conversation!(message.conversation_id).last_activity_at !=
+               nil
     end
 
     test "create_message/1 with invalid source returns error changeset" do
