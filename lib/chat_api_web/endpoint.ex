8e6c3f8ee2791c0479cb3fa70a4703df28778d15
reichert621 <reichertjalex@gmail.com>
2021-03-16 19:00:07 -0400

    Try again

--- lib/chat_api_web/endpoint.ex
+++ lib/chat_api_web/endpoint.ex
@@ -22,7 +22,7 @@ defmodule ChatApiWeb.Endpoint do
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug(Plug.Static, at: "/", from: :chat_api, gzip: true)
+  plug(Plug.Static, at: "/", from: :chat_api, gzip: true, only: ~w(static js css))
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
