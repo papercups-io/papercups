ebf4944d612c5dc91d175b36924266f989da58f6
Andy <andy@gettimesaved.com>
2021-03-16 22:28:08 -0400

    Change tag controller to default as authorized

--- lib/chat_api_web/controllers/tag_controller.ex
+++ lib/chat_api_web/controllers/tag_controller.ex
@@ -6,7 +6,7 @@ defmodule ChatApiWeb.TagController do
 
   action_fallback ChatApiWeb.FallbackController
 
-  plug :authorize when action in [:show, :update, :delete]
+  plug :authorize when action not in [:index, :create]
 
   defp authorize(conn, _) do
     id = conn.path_params["id"]
