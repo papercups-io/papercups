9774fcb917dcfce1dc9da6663d68329d1d80ff07
Andy Tran <andy@gettimesaved.com>
2021-03-19 16:20:09 -0400

    Add authorize plug to tag and company controllers (#649)
    * Add authorize plug to tag controller
    
    * Remove unused variable
    
    * Change tag controller to default as authorized
    
    * Change string in test
    
    * Add authorization plug to companies
    
    * Update lib/chat_api_web/controllers/company_controller.ex
    
    * Update lib/chat_api_web/controllers/tag_controller.ex
    
    Co-authored-by: Alex Reichert <reichertjalex@gmail.com>

--- lib/chat_api_web/controllers/company_controller.ex
+++ lib/chat_api_web/controllers/company_controller.ex
@@ -6,6 +6,19 @@ defmodule ChatApiWeb.CompanyController do
 
   action_fallback ChatApiWeb.FallbackController
 
+  plug :authorize when action in [:show, :update, :delete]
+
+  defp authorize(conn, _) do
+    id = conn.path_params["id"]
+
+    with %{account_id: account_id} <- conn.assigns.current_user,
+         company = %{account_id: ^account_id} <- Companies.get_company!(id) do
+      assign(conn, :current_company, company)
+    else
+      _ -> ChatApiWeb.FallbackController.call(conn, {:error, :not_found}) |> halt()
+    end
+  end
+
   @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     with %{account_id: account_id} <- conn.assigns.current_user do
@@ -29,25 +42,21 @@ defmodule ChatApiWeb.CompanyController do
   end
 
   @spec show(Plug.Conn.t(), map()) :: Plug.Conn.t()
-  def show(conn, %{"id" => id}) do
-    company = Companies.get_company!(id)
-    render(conn, "show.json", company: company)
+  def show(conn, %{"id" => _id}) do
+    render(conn, "show.json", company: conn.assigns.current_company)
   end
 
   @spec update(Plug.Conn.t(), map()) :: Plug.Conn.t()
-  def update(conn, %{"id" => id, "company" => company_params}) do
-    company = Companies.get_company!(id)
-
-    with {:ok, %Company{} = company} <- Companies.update_company(company, company_params) do
+  def update(conn, %{"id" => _id, "company" => company_params}) do
+    with {:ok, %Company{} = company} <-
+           Companies.update_company(conn.assigns.current_company, company_params) do
       render(conn, "show.json", company: company)
     end
   end
 
   @spec delete(Plug.Conn.t(), map()) :: Plug.Conn.t()
-  def delete(conn, %{"id" => id}) do
-    company = Companies.get_company!(id)
-
-    with {:ok, %Company{}} <- Companies.delete_company(company) do
+  def delete(conn, %{"id" => _id}) do
+    with {:ok, %Company{}} <- Companies.delete_company(conn.assigns.current_company) do
       send_resp(conn, :no_content, "")
     end
   end
