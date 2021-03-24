816210e99d4a13f4336f458ac4467634ee80aab0
reichert621 <reichertjalex@gmail.com>
2021-03-23 22:44:43 -0400

    More log info for message notification debugging

--- lib/chat_api/messages/notification.ex
+++ lib/chat_api/messages/notification.ex
@@ -10,6 +10,10 @@ defmodule ChatApi.Messages.Notification do
 
   @spec broadcast_to_customer!(Message.t()) :: Message.t()
   def broadcast_to_customer!(%Message{private: false} = message) do
+    Logger.info(
+      "Sending message notification: broadcast_to_customer! (message #{inspect(message.id)})"
+    )
+
     message
     |> Helpers.get_conversation_topic()
     |> ChatApiWeb.Endpoint.broadcast!("shout", Helpers.format(message))
@@ -21,6 +25,10 @@ defmodule ChatApi.Messages.Notification do
 
   @spec broadcast_to_admin!(Message.t()) :: Message.t()
   def broadcast_to_admin!(%Message{} = message) do
+    Logger.info(
+      "Sending message notification: broadcast_to_admin! (message #{inspect(message.id)})"
+    )
+
     message
     |> Helpers.get_admin_topic()
     |> ChatApiWeb.Endpoint.broadcast!("shout", Helpers.format(message))
@@ -36,7 +44,7 @@ defmodule ChatApi.Messages.Notification do
         :slack,
         opts
       ) do
-    Logger.info("Sending message notification: :slack")
+    Logger.info("Sending message notification: :slack (message #{inspect(message.id)})")
 
     case opts do
       [metadata: %{"send_to_reply_channel" => false}] ->
@@ -55,7 +63,7 @@ defmodule ChatApi.Messages.Notification do
   end
 
   def notify(%Message{account_id: account_id} = message, :webhooks, _opts) do
-    Logger.info("Sending message notification: :webhooks")
+    Logger.info("Sending message notification: :webhooks (message #{inspect(message.id)})")
     # TODO: how should we handle errors/retry logic?
     Task.start(fn ->
       EventSubscriptions.notify_event_subscriptions(account_id, %{
@@ -68,7 +76,10 @@ defmodule ChatApi.Messages.Notification do
   end
 
   def notify(%Message{} = message, :new_message_email, _opts) do
-    Logger.info("Sending message notification: :new_message_email")
+    Logger.info(
+      "Sending message notification: :new_message_email (message #{inspect(message.id)})"
+    )
+
     # TODO: how should we handle errors/retry logic?
     Task.start(fn ->
       ChatApi.Emails.send_new_message_alerts(message)
@@ -78,7 +89,7 @@ defmodule ChatApi.Messages.Notification do
   end
 
   def notify(%Message{} = message, :mattermost, _opts) do
-    Logger.info("Sending message notification: :mattermost")
+    Logger.info("Sending message notification: :mattermost (message #{inspect(message.id)})")
 
     Task.start(fn ->
       ChatApi.Mattermost.Notification.notify_primary_channel(message)
@@ -88,7 +99,10 @@ defmodule ChatApi.Messages.Notification do
   end
 
   def notify(%Message{private: false} = message, :conversation_reply_email, _opts) do
-    Logger.info("Sending message notification: :conversation_reply_email")
+    Logger.info(
+      "Sending message notification: :conversation_reply_email (message #{inspect(message.id)})"
+    )
+
     # 20 minutes (TODO: make this configurable?)
     schedule_in = 20 * 60
     formatted = Helpers.format(message)
@@ -105,7 +119,9 @@ defmodule ChatApi.Messages.Notification do
   end
 
   def notify(%Message{private: false} = message, :slack_company_channel, _opts) do
-    Logger.info("Sending message notification: :slack_company_channel")
+    Logger.info(
+      "Sending message notification: :slack_company_channel (message #{inspect(message.id)})"
+    )
 
     Task.start(fn ->
       ChatApi.Slack.Notification.notify_company_channel(message)
@@ -116,7 +132,9 @@ defmodule ChatApi.Messages.Notification do
 
   # TODO: come up with a better name... it's not super clear what `slack_support_channel` means!
   def notify(%Message{private: false} = message, :slack_support_channel, _opts) do
-    Logger.info("Sending message notification: :slack_support_channel")
+    Logger.info(
+      "Sending message notification: :slack_support_channel (message #{inspect(message.id)})"
+    )
 
     Task.start(fn ->
       ChatApi.Slack.Notification.notify_support_channel(message)
