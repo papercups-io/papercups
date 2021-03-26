dc81c2fd4217d5705a9931097c912abd08ed1aaa
reichert621 <reichertjalex@gmail.com>
2021-03-26 09:57:08 -0400

    Fix migration

--- priv/repo/migrations/20210326132156_fix_tag_name_index.exs
+++ priv/repo/migrations/20210326132156_fix_tag_name_index.exs
@@ -2,12 +2,11 @@ defmodule ChatApi.Repo.Migrations.FixTagNameIndex do
   use Ecto.Migration
 
   def up do
-    drop(unique_index(:tags, [:name]))
+    drop_if_exists(unique_index(:tags, [:name]))
     create(unique_index(:tags, [:name, :account_id]))
   end
 
   def down do
     drop(unique_index(:tags, [:name, :account_id]))
-    create(unique_index(:tags, [:name]))
   end
 end
