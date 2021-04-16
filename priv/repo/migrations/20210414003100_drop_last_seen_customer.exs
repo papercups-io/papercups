463c1f91c8f137bf0717aaf3c7cc26cdb16d1761
Utsa Varia <utsav99varia@gmail.com>
2021-04-17 02:58:32 +0530

    Added migration to auto populate last_seen_at field with last_seen field for customers

--- priv/repo/migrations/20210414003100_drop_last_seen_customer.exs
+++ priv/repo/migrations/20210414003100_drop_last_seen_customer.exs
@@ -1,7 +1,17 @@
 defmodule ChatApi.Repo.Migrations.DropCusotmerLastSeen do
   use Ecto.Migration
+  import Ecto.Query
+
+  alias ChatApi.Repo
 
   def change do
+    from(
+      c in "customers",
+      where: is_nil(c.last_seen_at),
+      update: [set: [last_seen_at: c.last_seen]]
+    )
+    |> Repo.update_all([])
+
     alter table(:customers) do
       remove :last_seen
     end
