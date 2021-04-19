d83e4e585875e7290574834bad768a272ea4d8d8
reichert621 <reichertjalex@gmail.com>
2021-04-19 15:27:25 -0400

    Clena up GithubAuthorizationView

--- lib/chat_api_web/views/github_authorization_view.ex
+++ lib/chat_api_web/views/github_authorization_view.ex
@@ -3,20 +3,25 @@ defmodule ChatApiWeb.GithubAuthorizationView do
   alias ChatApiWeb.GithubAuthorizationView
 
   def render("index.json", %{github_authorizations: github_authorizations}) do
-    %{data: render_many(github_authorizations, GithubAuthorizationView, "github_authorization.json")}
+    %{
+      data:
+        render_many(github_authorizations, GithubAuthorizationView, "github_authorization.json")
+    }
   end
 
   def render("show.json", %{github_authorization: github_authorization}) do
-    %{data: render_one(github_authorization, GithubAuthorizationView, "github_authorization.json")}
+    %{
+      data: render_one(github_authorization, GithubAuthorizationView, "github_authorization.json")
+    }
   end
 
   def render("github_authorization.json", %{github_authorization: github_authorization}) do
-    %{id: github_authorization.id,
+    %{
+      id: github_authorization.id,
       token_type: github_authorization.token_type,
-      access_token: github_authorization.access_token,
       scope: github_authorization.scope,
       user_id: github_authorization.user_id,
-      account_id: github_authorization.account_id,
-      metadata: github_authorization.metadata}
+      account_id: github_authorization.account_id
+    }
   end
 end
