16a9c822a86446c2d86b0649856c497b92b9962b
Alex Reichert <reichertjalex@gmail.com>
2021-05-05 19:00:07 -0400

    Add more functionality to hubspot api client (#819)

--- lib/chat_api/hubspot/client.ex
+++ lib/chat_api/hubspot/client.ex
@@ -7,39 +7,103 @@ defmodule ChatApi.Hubspot.Client do
 
   use Tesla
 
-  plug Tesla.Middleware.BaseUrl, "https://api.hubapi.com"
+  plug(Tesla.Middleware.BaseUrl, "https://api.hubapi.com")
 
-  plug Tesla.Middleware.Headers, [
+  plug(Tesla.Middleware.Headers, [
     {"content-type", "application/json; charset=utf-8"}
-  ]
+  ])
 
-  plug Tesla.Middleware.JSON
-  plug Tesla.Middleware.Logger
+  plug(Tesla.Middleware.JSON)
+  plug(Tesla.Middleware.Logger)
 
-  def list_contacts_v1(access_token) do
+  # TODO: determine whether v1 or v3 API is more reliable/feature-rich
+
+  def list_contacts_v1(access_token, query \\ [count: 100]) do
     get("/contacts/v1/lists/all/contacts/all",
-      query: [count: 5],
+      query: query,
       headers: [
         {"Authorization", "Bearer " <> access_token}
       ]
     )
   end
 
-  def list_contacts_v3(access_token) do
+  def list_contacts(access_token, query \\ [limit: 100]) do
     get("/crm/v3/objects/contacts",
-      query: [limit: 5],
+      query: query,
       headers: [
         {"Authorization", "Bearer " <> access_token}
       ]
     )
+  end
+
+  def retrieve_contact(access_token, contact_id, query \\ []) do
+    get("/crm/v3/objects/contacts/#{contact_id}",
+      query: query,
+      headers: [
+        {"Authorization", "Bearer " <> access_token}
+      ]
+    )
+  end
+
+  def create_contact(access_token, properties \\ %{}) do
+    post(
+      "/crm/v3/objects/contacts",
+      %{
+        "properties" => %{
+          "company" => Map.get(properties, "company"),
+          "email" => Map.get(properties, "email"),
+          "firstname" => Map.get(properties, "first_name"),
+          "lastname" => Map.get(properties, "last_name"),
+          "phone" => Map.get(properties, "phone"),
+          "website" => Map.get(properties, "website")
+        }
+      },
+      headers: [
+        {"Authorization", "Bearer " <> access_token}
+      ]
+    )
+  end
+
+  def search_contacts(access_token, filters \\ []) do
+    post("/crm/v3/objects/contacts/search", %{"filters" => filters},
+      headers: [
+        {"Authorization", "Bearer " <> access_token}
+      ]
+    )
+  end
+
+  def find_contact_by_email(access_token, email) do
+    search_contacts(
+      [
+        %{
+          "propertyName" => "email",
+          "operator" => "EQ",
+          "value" => email
+        }
+      ],
+      access_token
+    )
+  end
+
+  def refresh_auth_tokens(refresh_token) do
+    create_auth_tokens(%{
+      "grant_type" => "refresh_token",
+      "refresh_token" => refresh_token
+    })
   end
 
   def generate_auth_tokens(code) do
+    create_auth_tokens(%{
+      "grant_type" => "authorization_code",
+      "redirect_uri" => System.get_env("PAPERCUPS_HUBSPOT_REDIRECT_URI"),
+      "code" => code
+    })
+  end
+
+  def create_auth_tokens(params) do
     # app_id = System.get_env("PAPERCUPS_HUBSPOT_APP_ID")
-    grant_type = "authorization_code"
     client_id = System.get_env("PAPERCUPS_HUBSPOT_CLIENT_ID")
     client_secret = System.get_env("PAPERCUPS_HUBSPOT_CLIENT_SECRET")
-    redirect_uri = System.get_env("PAPERCUPS_HUBSPOT_REDIRECT_URI")
 
     [
       {Tesla.Middleware.BaseUrl, "https://api.hubapi.com"},
@@ -50,12 +114,12 @@ defmodule ChatApi.Hubspot.Client do
        [{"Content-Type", "application/x-www-form-urlencoded;charset=utf-8"}]}
     ]
     |> Tesla.client()
-    |> Tesla.post("/oauth/v1/token", %{
-      "grant_type" => grant_type,
-      "client_id" => client_id,
-      "client_secret" => client_secret,
-      "redirect_uri" => redirect_uri,
-      "code" => code
-    })
+    |> Tesla.post(
+      "/oauth/v1/token",
+      Map.merge(params, %{
+        "client_id" => client_id,
+        "client_secret" => client_secret
+      })
+    )
   end
 end
