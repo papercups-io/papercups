751aecde8c1ce2ce0f8970b41c2029655cb145ac
reichert621 <reichertjalex@gmail.com>
2021-05-17 13:30:13 -0400

    Fix migration script

--- priv/repo/migrations/20210517172713_remove_customer_last_seen_requirement.exs
+++ priv/repo/migrations/20210517172713_remove_customer_last_seen_requirement.exs
@@ -1,10 +1,9 @@
-defmodule ChatApi.Repo.Migrations.ModifyCusotmerLastSeen do
+defmodule ChatApi.Repo.Migrations.RemoveCustomerLastSeenRequirement do
   use Ecto.Migration
 
   def change do
     alter table(:customers) do
       modify(:last_seen, :date, null: true)
-      modify(:last_seen_at, :utc_datetime, null: false)
     end
   end
 end
