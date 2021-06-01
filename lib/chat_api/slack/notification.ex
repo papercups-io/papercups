0db1749513169f7ff12995ee72d576a24fa9d22c
reichert621 <reichertjalex@gmail.com>
2021-06-01 11:53:05 -0400

    Sync all conversation messages to slack reply channel if necessary

--- lib/chat_api/slack/notification.ex
+++ lib/chat_api/slack/notification.ex
@@ -14,7 +14,9 @@ defmodule ChatApi.Slack.Notification do
     Messages.Message
   }
 
+  alias ChatApi.Conversations.Conversation
   alias ChatApi.Users.{User, UserProfile}
+  alias ChatApi.SlackAuthorizations.SlackAuthorization
   alias ChatApi.SlackConversationThreads.SlackConversationThread
 
   @spec log(binary()) :: :ok | Tesla.Env.result()
@@ -49,51 +51,30 @@ defmodule ChatApi.Slack.Notification do
       ) do
     Logger.info("Calling ChatApi.Slack.Notification.notify_primary_channel")
     # TODO: handle getting all these fields in a separate function?
-    with %{customer: customer} = conversation <-
+    with %Conversation{} = conversation <-
            Conversations.get_conversation_with!(conversation_id, :customer),
-         %{access_token: access_token, channel: channel, channel_id: channel_id} = authorization <-
+         %SlackAuthorization{channel_id: channel_id} = authorization <-
            SlackAuthorizations.get_authorization_by_account(account_id, %{type: "reply"}),
          is_first_message <-
            Conversations.is_first_message?(conversation_id, message_id),
          thread <-
            SlackConversationThreads.get_thread_by_conversation_id(conversation_id, channel_id),
          :ok <- validate_send_to_primary_channel(thread, is_first_message: is_first_message) do
-      # TODO: use a struct here?
-      %{
-        conversation: conversation,
-        message: message,
-        authorization: authorization,
-        thread: thread
-      }
-      |> Slack.Helpers.get_message_text()
-      |> Slack.Helpers.get_message_payload(%{
-        channel: channel,
-        conversation: conversation,
-        customer: customer,
-        thread: thread,
-        message: message
-      })
-      |> Slack.Client.send_message(access_token)
-      |> case do
-        # Just pass through in test/dev mode (not sure if there's a more idiomatic way to do this)
-        {:ok, nil} ->
-          nil
+      case validate_send_to_primary_channel(thread, is_first_message: is_first_message) do
+        :ok ->
+          send_to_primary_channel(%{
+            conversation: conversation,
+            message: message,
+            authorization: authorization,
+            thread: thread
+          })
 
-        {:ok, response} ->
-          # If no thread exists yet, start a new thread and kick off the first reply
-          if is_nil(thread) do
-            {:ok, thread} =
-              Slack.Helpers.create_new_slack_conversation_thread(conversation_id, response)
-
-            Slack.Client.send_message(
-              %{
-                "channel" => channel,
-                "text" => "(Send a message here to get started!)",
-                "thread_ts" => thread.slack_thread_ts
-              },
-              access_token
-            )
-          end
+        {:error, :conversation_exists_without_thread} ->
+          sync_conversation_messages_to_primary_channel(%{
+            conversation: conversation,
+            authorization: authorization,
+            thread: thread
+          })
 
         error ->
           Logger.error("Unable to send Slack message: #{inspect(error)}")
@@ -102,6 +83,87 @@ defmodule ChatApi.Slack.Notification do
       error ->
         Logger.info("Skipped sending Slack message: #{inspect(error)}")
     end
+  end
+
+  @spec send_to_primary_channel(%{
+          :authorization => SlackAuthorization.t(),
+          :conversation => Conversation.t(),
+          :message => Message.t(),
+          :thread => nil | SlackConversationThread.t()
+        }) :: nil | :ok | {:error, any} | {:ok, nil | Tesla.Env.t()}
+  def send_to_primary_channel(%{
+        conversation: %Conversation{id: conversation_id, customer: customer} = conversation,
+        message: message,
+        authorization:
+          %SlackAuthorization{access_token: access_token, channel: channel} = authorization,
+        thread: thread
+      }) do
+    %{
+      conversation: conversation,
+      message: message,
+      authorization: authorization,
+      thread: thread
+    }
+    |> Slack.Helpers.get_message_text()
+    |> Slack.Helpers.get_message_payload(%{
+      channel: channel,
+      conversation: conversation,
+      customer: customer,
+      thread: thread,
+      message: message
+    })
+    |> Slack.Client.send_message(access_token)
+    |> case do
+      # Just pass through in test/dev mode (not sure if there's a more idiomatic way to do this)
+      {:ok, nil} ->
+        nil
+
+      {:ok, response} ->
+        # If no thread exists yet, start a new thread and kick off the first reply
+        if is_nil(thread) do
+          {:ok, thread} =
+            Slack.Helpers.create_new_slack_conversation_thread(conversation_id, response)
+
+          Slack.Client.send_message(
+            %{
+              "channel" => channel,
+              "text" => "(Send a message here to get started!)",
+              "thread_ts" => thread.slack_thread_ts
+            },
+            access_token
+          )
+        end
+
+      error ->
+        Logger.error("Unable to send Slack message: #{inspect(error)}")
+    end
+  end
+
+  @spec sync_conversation_messages_to_primary_channel(%{
+          :authorization => SlackAuthorization.t(),
+          :conversation => Conversation.t(),
+          :thread => nil | SlackConversationThread.t()
+        }) :: :ok
+  def sync_conversation_messages_to_primary_channel(%{
+        conversation: %Conversation{id: conversation_id} = conversation,
+        authorization: authorization,
+        thread: thread
+      }) do
+    conversation_id
+    |> Conversations.get_conversation!()
+    |> Map.get(:messages, [])
+    |> Enum.sort_by(&{&1.inserted_at, &1.sent_at}, NaiveDateTime)
+    |> Enum.each(fn message ->
+      send_to_primary_channel(%{
+        conversation: conversation,
+        message: message,
+        authorization: authorization,
+        thread: thread
+      })
+
+      # TODO: how should we handle this?
+      Process.sleep(200)
+    end)
   end
 
   @spec notify_support_channel(Message.t()) :: :ok
@@ -169,10 +231,13 @@ defmodule ChatApi.Slack.Notification do
   # Otherwise, return :error (i.e. we don't want to start a new thread with a non-initial message)
   @spec validate_send_to_primary_channel(SlackConversationThread.t() | nil, [
           {:is_first_message, boolean}
-        ]) :: :error | :ok
+        ]) :: :ok | {:error, atom()}
+  def validate_send_to_primary_channel(nil, is_first_message: false),
+    do: {:error, :conversation_exists_without_thread}
+
   def validate_send_to_primary_channel(_thread, is_first_message: true), do: :ok
   def validate_send_to_primary_channel(%SlackConversationThread{}, _opts), do: :ok
-  def validate_send_to_primary_channel(_thread, _opts), do: :error
+  def validate_send_to_primary_channel(_thread, _opts), do: {:error, :unexpected_thread_state}
 
   # TODO: maybe these methods below belong in the Slack.Helpers module?
 
