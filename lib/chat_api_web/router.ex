a56fda936a52c88724d523a8a50adbf3718b994b
Alex Reichert <reichertjalex@gmail.com>
2021-06-04 11:49:15 -0400

    Phoenix analytics (#860)
    * Test phoenix live dashboard
    
    * Scope under admin
    
    * Add todo comment

--- lib/chat_api_web/router.ex
+++ lib/chat_api_web/router.ex
@@ -169,13 +169,12 @@ defmodule ChatApiWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  import Phoenix.LiveDashboard.Router
 
-    scope "/" do
-      pipe_through([:fetch_session, :protect_from_forgery])
-      live_dashboard("/dashboard", metrics: ChatApiWeb.Telemetry)
-    end
+  # TODO: add authentication after testing
+  scope "/admin" do
+    pipe_through([:fetch_session, :protect_from_forgery])
+    live_dashboard("/dashboard", metrics: ChatApiWeb.Telemetry)
   end
 
   if Mix.env() == :dev do
