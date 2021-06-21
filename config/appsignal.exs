01a05a3890c140fb3d0818f6ce6274772f9168f0
reichert621 <reichertjalex@gmail.com>
2021-06-21 16:07:29 -0400

    Test removing all but runtime config

--- config/appsignal.exs
+++
@@ -1,7 +0,0 @@
-use Mix.Config
-
-config :appsignal, :config,
-  otp_app: :chat_api,
-  name: "chat_api",
-  push_api_key: System.get_env("APPSIGNAL_API_KEY"),
-  env: Mix.env()
--- config/config.exs
+++ config/config.exs
@@ -79,5 +79,3 @@ config :chat_api, ChatApi.Mailers.Gmail, adapter: Swoosh.Adapters.Gmail
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
-
-import_config "appsignal.exs"
