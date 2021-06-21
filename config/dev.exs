3807ec4770d828d04fd851c48b6454da8c0828b9
reichert621 <reichertjalex@gmail.com>
2021-06-21 14:11:15 -0400

    Try adding appsignal config to runtime.exs

--- config/dev.exs
+++ config/dev.exs
@@ -73,4 +73,4 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
-config :appsignal, :config, active: false
+config :appsignal, :config, active: true
