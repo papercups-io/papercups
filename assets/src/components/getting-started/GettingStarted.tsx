5284d212ac08c51f0c4a809f08d3aa05530a2a9e
reichert621 <reichertjalex@gmail.com>
2021-06-25 14:48:41 -0400

    Minor refactoring

--- assets/src/components/getting-started/GettingStarted.tsx
+++ assets/src/components/getting-started/GettingStarted.tsx
@@ -4,7 +4,7 @@ import {Box, Flex, Spinner} from 'theme-ui';
 import * as API from '../../api';
 import type {OnboardingStatus} from '../../types';
 import logger from '../../logger';
-import {Divider, Title} from '../common';
+import {Container, Divider, Title} from '../common';
 import Steps from './Steps';
 
 const GettingStarted = () => {
@@ -25,25 +25,29 @@ const GettingStarted = () => {
     })();
   }, []);
 
-  return loading ? (
-    <Flex
-      sx={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-      }}
-    >
-      <Spinner size={40} />
-    </Flex>
-  ) : (
-    <Box p={4} sx={{maxWidth: 1080}}>
+  if (loading) {
+    return (
+      <Flex
+        sx={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}
+      >
+        <Spinner size={40} />
+      </Flex>
+    );
+  }
+
+  return (
+    <Container>
       <Box mb={4}>
         <Title level={3}>Welcome to Papercups</Title>
       </Box>
       <Divider />
       <Steps onboardingStatus={onboardingStatus} />
-    </Box>
+    </Container>
   );
 };
 
