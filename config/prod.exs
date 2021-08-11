ef954fd18362a9bb48e3789dba2b2c3835213de4
cheeseblubber <kamleung911@gmail.com>
2021-08-11 14:56:09 -0400

    fix syntax

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,8 @@
+use Mix.Config
 # Do not print debug messages in production
 config :logger, level: :info
 
 # Heroku needs ssl to be set to true and it doesn't run
 config :chat_api, ChatApi.Repo,
   show_sensitive_data_on_connection_error: true,
-  socket_options: [:inet],
+  socket_options: [:inet]
