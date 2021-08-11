cd823674420132264b4379bcfb535091bf5038e1
cheeseblubber <kamleung911@gmail.com>
2021-08-11 14:53:48 -0400

    experiment with prod file

--- config/prod.exs
+++ config/prod.exs
@@ -1,14 +1,3 @@
-# Do not delete mix still looks for prod.exs other wise heroku will fail even though configs are set in runtime.exs
-database_url = System.get_env("DATABASE_URL")
-pool_size = String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-require_db_ssl =
-  case System.get_env("REQUIRE_DB_SSL") do
-    "true" -> true
-    "false" -> false
-    _ -> true
-  end
-
 # Do not print debug messages in production
 config :logger, level: :info
 
