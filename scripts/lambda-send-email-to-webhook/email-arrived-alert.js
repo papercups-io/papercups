24bcaaba05b4236d1778057271b32cd154aea012
cheeseblubber <kamleung911@gmail.com>
2021-08-24 16:18:12 -0400

    add deps for ses lambda function

---
+++ scripts/lambda-send-email-to-webhook/email-arrived-alert.js
@@ -0,0 +1,41 @@
+//Using js file since lambda function doesn't support typescript by default
+//to use ts in lambda you have to set up custom runtime (April 5, 2020)
+const request = require("requestretry");
+
+const parseFromAddress = (email) => {
+  const found = email.match("<(.*?)>");
+  if (!found[1]) {
+    throw Error("invalid email address: " + email);
+  }
+  return found[1];
+};
+
+const handler = async (event) => {
+  const url = process.env.WEBHOOK_URL;
+  if (!url) {
+    throw Error("MISSING WEBHOOK URL ENVIRONMENT VARIABLE");
+  }
+
+  const mail = event.Records[0].ses.mail;
+  const messageId = mail.messageId;
+  const toAddresses = mail.commonHeaders.to;
+  const fromAddress = mail.commonHeaders.from;
+
+  if (!messageId || !toAddresses || !fromAddress) {
+    throw Error("Missing parameter in message");
+  }
+
+  const params = {
+    url: url + "/api/ses/webhook",
+    body: {
+      messageId,
+      toAddresses,
+      fromAddress: fromAddress.map((addr) => parseFromAddress(addr)),
+    },
+    json: true,
+  };
+
+  return await request.post(params);
+};
+
+exports.handler = handler;
---
+++ scripts/ses_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# set up ses function
+cd lambda-send-email-to-webhook || exit
+
+npm install
+
+# REQUIREMENT TO CREATE A LAMBDA ROLE: See: https://aws.amazon.com/blogs/messaging-and-targeting/forward-incoming-email-to-an-external-destination/
+
+zip -r function.zip .
+aws lambda create-function --function-name "send-email-to-webhook" \
+    --runtime 'nodejs14.x' \
+    --handler "email-arrived-alert.handler" \
+    --role "arn:aws:iam::775327867774:role/lambda-s3-role" \
+    --zip-file fileb://function.zip \
+    --environment "Variables={WEBHOOK_URL=http://df15-184-152-2-44.ngrok.io}" \
+    --handler "email-arrived-alert.handler"
+
+# You may need to add a resource based policy that allows ses to trigger the lambda function from your aws account
+
+rm function.zip
