9981a04576b7ccc3bd4adb966ae70757af8a190f
reichert621 <reichertjalex@gmail.com>
2021-09-16 13:35:35 -0400

    Comment out unused code for now

--- assets/src/components/inboxes/InboxConversations.tsx
+++ assets/src/components/inboxes/InboxConversations.tsx
@@ -61,310 +61,312 @@ const getNextSelectedConversationId = (
   }
 };
 
-export const InboxConversations = ({
-  inbox,
-  account,
-  currentUser,
-  filter = {},
-  isValidConversation = defaultConversationFilter,
-}: {
-  inbox: Inbox;
-  account: Account;
-  currentUser: User;
-  filter: Record<string, any>;
-  isValidConversation: (conversation: Conversation) => boolean;
-}) => {
-  const {id: inboxId} = inbox;
-  const scrollToEl = React.useRef<any>(null);
-  const [status, setStatus] = React.useState<'loading' | 'success' | 'error'>(
-    'loading'
-  );
-  const [error, setErrorMessage] = React.useState<string | null>(null);
-  const [conversationIds, setConversationIds] = React.useState<Array<string>>(
-    []
-  );
-  const [pagination, setPaginationOptions] = React.useState<
-    API.PaginationOptions
-  >({});
-  const [selectedConversationId, setSelectedConversationId] = React.useState<
-    string | null
-  >(null);
-  const [closing, setClosingConversations] = React.useState<Array<string>>([]);
+// TODO: verify that ConversationsDashboard actually works as a substitute
 
-  const {
-    fetchConversations,
-    getValidConversations,
-    getConversationById,
-    getMessagesByConversationId,
-    updateConversationById,
-    archiveConversationById,
-  } = useConversations();
+// export const InboxConversations = ({
+//   inbox,
+//   account,
+//   currentUser,
+//   filter = {},
+//   isValidConversation = defaultConversationFilter,
+// }: {
+//   inbox: Inbox;
+//   account: Account;
+//   currentUser: User;
+//   filter: Record<string, any>;
+//   isValidConversation: (conversation: Conversation) => boolean;
+// }) => {
+//   const {id: inboxId} = inbox;
+//   const scrollToEl = React.useRef<any>(null);
+//   const [status, setStatus] = React.useState<'loading' | 'success' | 'error'>(
+//     'loading'
+//   );
+//   const [error, setErrorMessage] = React.useState<string | null>(null);
+//   const [conversationIds, setConversationIds] = React.useState<Array<string>>(
+//     []
+//   );
+//   const [pagination, setPaginationOptions] = React.useState<
+//     API.PaginationOptions
+//   >({});
+//   const [selectedConversationId, setSelectedConversationId] = React.useState<
+//     string | null
+//   >(null);
+//   const [closing, setClosingConversations] = React.useState<Array<string>>([]);
 
-  const {handleSendMessage, handleConversationSeen} = useNotifications();
+//   const {
+//     fetchConversations,
+//     getValidConversations,
+//     getConversationById,
+//     getMessagesByConversationId,
+//     updateConversationById,
+//     archiveConversationById,
+//   } = useConversations();
 
-  const {users = []} = account;
-  // TODO: is there a more efficient way to do this?
-  const conversations = getValidConversations(isValidConversation);
-  const hasMoreConversations =
-    !!pagination.next &&
-    !!pagination.total &&
-    conversations.length < pagination.total;
-  const isClosingSelected =
-    !!selectedConversationId && closing.indexOf(selectedConversationId) !== -1;
-  const conversation = getConversationById(selectedConversationId);
-  const messages = getMessagesByConversationId(selectedConversationId);
+//   const {handleSendMessage, handleConversationSeen} = useNotifications();
 
-  React.useEffect(() => {
-    setStatus('loading');
+//   const {users = []} = account;
+//   // TODO: is there a more efficient way to do this?
+//   const conversations = getValidConversations(isValidConversation);
+//   const hasMoreConversations =
+//     !!pagination.next &&
+//     !!pagination.total &&
+//     conversations.length < pagination.total;
+//   const isClosingSelected =
+//     !!selectedConversationId && closing.indexOf(selectedConversationId) !== -1;
+//   const conversation = getConversationById(selectedConversationId);
+//   const messages = getMessagesByConversationId(selectedConversationId);
 
-    fetchFilteredConversations()
-      .then((result) => {
-        const {data: conversations, ...pagination} = result;
-        const conversationIds = conversations.map((c) => c.id);
-        const [first] = conversationIds;
-        // TODO: should we handle conversation IDs and pagination options here,
-        // or in the ConversationsProvider? (Might need to keep pagination here)
-        setConversationIds(conversationIds);
-        setPaginationOptions(pagination);
-        handleSelectConversation(first || null);
-      })
-      .then(() => setStatus('success'))
-      .catch((error) => {
-        setStatus('error');
-        setErrorMessage(formatServerError(error));
-      });
-    // FIXME?
-    // eslint-disable-next-line
-  }, [inboxId]);
+//   React.useEffect(() => {
+//     setStatus('loading');
 
-  React.useEffect(() => {
-    scrollToEl.current?.scrollIntoView();
-  }, [inboxId, selectedConversationId, messages.length]);
+//     fetchFilteredConversations()
+//       .then((result) => {
+//         const {data: conversations, ...pagination} = result;
+//         const conversationIds = conversations.map((c) => c.id);
+//         const [first] = conversationIds;
+//         // TODO: should we handle conversation IDs and pagination options here,
+//         // or in the ConversationsProvider? (Might need to keep pagination here)
+//         setConversationIds(conversationIds);
+//         setPaginationOptions(pagination);
+//         handleSelectConversation(first || null);
+//       })
+//       .then(() => setStatus('success'))
+//       .catch((error) => {
+//         setStatus('error');
+//         setErrorMessage(formatServerError(error));
+//       });
+//     // FIXME?
+//     // eslint-disable-next-line
+//   }, [inboxId]);
 
-  function setScrollRef(el: any) {
-    scrollToEl.current = el || null;
-    scrollToEl.current?.scrollIntoView();
-  }
+//   React.useEffect(() => {
+//     scrollToEl.current?.scrollIntoView();
+//   }, [inboxId, selectedConversationId, messages.length]);
 
-  function fetchFilteredConversations(params = {}) {
-    return fetchConversations({...filter, ...params});
-  }
+//   function setScrollRef(el: any) {
+//     scrollToEl.current = el || null;
+//     scrollToEl.current?.scrollIntoView();
+//   }
 
-  function handleSelectConversation(conversationId: string | null) {
-    setSelectedConversationId(conversationId);
+//   function fetchFilteredConversations(params = {}) {
+//     return fetchConversations({...filter, ...params});
+//   }
 
-    if (!conversationId) {
-      return;
-    }
+//   function handleSelectConversation(conversationId: string | null) {
+//     setSelectedConversationId(conversationId);
 
-    const selected = getConversationById(conversationId);
+//     if (!conversationId) {
+//       return;
+//     }
 
-    if (selected && isUnreadConversation(selected, currentUser)) {
-      handleConversationSeen(conversationId);
-    }
+//     const selected = getConversationById(conversationId);
 
-    // TODO: history.push(/inboxes/:inbox_id/conversations/:conversation_id)
-  }
+//     if (selected && isUnreadConversation(selected, currentUser)) {
+//       handleConversationSeen(conversationId);
+//     }
 
-  async function handleLoadMoreConversations() {
-    const {data = [], ...nextPaginationOptions} = await fetchConversations({
-      after: pagination.next,
-    });
+//     // TODO: history.push(/inboxes/:inbox_id/conversations/:conversation_id)
+//   }
 
-    setConversationIds([
-      ...new Set([...conversationIds, ...data.map((c) => c.id)]),
-    ]);
-    setPaginationOptions(nextPaginationOptions);
-  }
+//   async function handleLoadMoreConversations() {
+//     const {data = [], ...nextPaginationOptions} = await fetchConversations({
+//       after: pagination.next,
+//     });
 
-  async function handleAssignUser(
-    conversationId: string,
-    userId: string | null
-  ) {
-    await updateConversationById(conversationId, {assignee_id: userId});
-  }
+//     setConversationIds([
+//       ...new Set([...conversationIds, ...data.map((c) => c.id)]),
+//     ]);
+//     setPaginationOptions(nextPaginationOptions);
+//   }
 
-  async function handleMarkPriority(conversationId: string) {
-    await updateConversationById(conversationId, {
-      priority: 'priority',
-    });
-  }
+//   async function handleAssignUser(
+//     conversationId: string,
+//     userId: string | null
+//   ) {
+//     await updateConversationById(conversationId, {assignee_id: userId});
+//   }
 
-  async function handleRemovePriority(conversationId: string) {
-    await updateConversationById(conversationId, {
-      priority: 'not_priority',
-    });
-  }
+//   async function handleMarkPriority(conversationId: string) {
+//     await updateConversationById(conversationId, {
+//       priority: 'priority',
+//     });
+//   }
 
-  async function handleCloseConversation(conversationId: string) {
-    setClosingConversations([...closing, conversationId]);
+//   async function handleRemovePriority(conversationId: string) {
+//     await updateConversationById(conversationId, {
+//       priority: 'not_priority',
+//     });
+//   }
 
-    const validConversationIds = conversations.map((c) => c.id);
-    const nextSelectedConversationId = getNextSelectedConversationId(
-      selectedConversationId,
-      validConversationIds
-    );
+//   async function handleCloseConversation(conversationId: string) {
+//     setClosingConversations([...closing, conversationId]);
 
-    // TODO: figure out the best way to handle this when closing multiple
-    // conversations in a row very quickly
-    await sleep(400);
-    await updateConversationById(conversationId, {status: 'closed'});
+//     const validConversationIds = conversations.map((c) => c.id);
+//     const nextSelectedConversationId = getNextSelectedConversationId(
+//       selectedConversationId,
+//       validConversationIds
+//     );
 
-    handleSelectConversation(nextSelectedConversationId);
-    setConversationIds(validConversationIds);
-    setClosingConversations(closing.filter((id) => id !== conversationId));
-  }
+//     // TODO: figure out the best way to handle this when closing multiple
+//     // conversations in a row very quickly
+//     await sleep(400);
+//     await updateConversationById(conversationId, {status: 'closed'});
 
-  async function handleReopenConversation(conversationId: string) {
-    const validConversationIds = conversations.map((c) => c.id);
-    const nextSelectedConversationId = getNextSelectedConversationId(
-      selectedConversationId,
-      validConversationIds
-    );
+//     handleSelectConversation(nextSelectedConversationId);
+//     setConversationIds(validConversationIds);
+//     setClosingConversations(closing.filter((id) => id !== conversationId));
+//   }
 
-    await updateConversationById(conversationId, {status: 'open'});
+//   async function handleReopenConversation(conversationId: string) {
+//     const validConversationIds = conversations.map((c) => c.id);
+//     const nextSelectedConversationId = getNextSelectedConversationId(
+//       selectedConversationId,
+//       validConversationIds
+//     );
 
-    notification.open({
-      message: 'Conversation re-opened!',
-      duration: 2, // 2 seconds
-      description: (
-        <Box>
-          You can view this conversations once again{' '}
-          <a href="/conversations/all">here</a>.
-        </Box>
-      ),
-    });
+//     await updateConversationById(conversationId, {status: 'open'});
 
-    await sleep(400);
+//     notification.open({
+//       message: 'Conversation re-opened!',
+//       duration: 2, // 2 seconds
+//       description: (
+//         <Box>
+//           You can view this conversations once again{' '}
+//           <a href="/conversations/all">here</a>.
+//         </Box>
+//       ),
+//     });
 
-    handleSelectConversation(nextSelectedConversationId);
-    setConversationIds(validConversationIds);
-  }
+//     await sleep(400);
 
-  async function handleDeleteConversation(conversationId: string) {
-    const validConversationIds = conversations.map((c) => c.id);
-    const nextSelectedConversationId = getNextSelectedConversationId(
-      selectedConversationId,
-      validConversationIds
-    );
+//     handleSelectConversation(nextSelectedConversationId);
+//     setConversationIds(validConversationIds);
+//   }
 
-    await archiveConversationById(conversationId);
+//   async function handleDeleteConversation(conversationId: string) {
+//     const validConversationIds = conversations.map((c) => c.id);
+//     const nextSelectedConversationId = getNextSelectedConversationId(
+//       selectedConversationId,
+//       validConversationIds
+//     );
 
-    notification.open({
-      message: 'Conversation deleted!',
-      duration: 2, // 2 seconds
-      description: (
-        <Box>
-          This conversation was permanently deleted. You can view your active
-          conversations <a href="/conversations/all">here</a>.
-        </Box>
-      ),
-    });
+//     await archiveConversationById(conversationId);
 
-    await sleep(400);
+//     notification.open({
+//       message: 'Conversation deleted!',
+//       duration: 2, // 2 seconds
+//       description: (
+//         <Box>
+//           This conversation was permanently deleted. You can view your active
+//           conversations <a href="/conversations/all">here</a>.
+//         </Box>
+//       ),
+//     });
 
-    handleSelectConversation(nextSelectedConversationId);
-    setConversationIds(validConversationIds);
-  }
+//     await sleep(400);
 
-  function handleSendNewMessage(message: Partial<Message>) {
-    if (!selectedConversationId) {
-      return;
-    }
+//     handleSelectConversation(nextSelectedConversationId);
+//     setConversationIds(validConversationIds);
+//   }
 
-    handleSendMessage({
-      conversation_id: selectedConversationId,
-      ...message,
-    });
-  }
+//   function handleSendNewMessage(message: Partial<Message>) {
+//     if (!selectedConversationId) {
+//       return;
+//     }
 
-  if (error) {
-    return (
-      <Flex
-        sx={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100%',
-        }}
-      >
-        <Result
-          status="error"
-          title="Error retrieving inbox"
-          subTitle={error || 'Unknown error'}
-        />
-      </Flex>
-    );
-  }
+//     handleSendMessage({
+//       conversation_id: selectedConversationId,
+//       ...message,
+//     });
+//   }
 
-  return (
-    <Layout style={{background: colors.white}}>
-      <Sider
-        theme="light"
-        width={CONVERSATIONS_DASHBOARD_SIDER_WIDTH}
-        style={{
-          borderRight: '1px solid #f0f0f0',
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: CONVERSATIONS_DASHBOARD_SIDER_OFFSET,
-        }}
-      >
-        <Box sx={{borderBottom: '1px solid #f0f0f0'}}>
-          <Box px={3} py={3}>
-            <Title level={3} style={{marginBottom: 0, marginTop: 8}}>
-              {inbox.name}
-            </Title>
-          </Box>
-        </Box>
+//   if (error) {
+//     return (
+//       <Flex
+//         sx={{
+//           flex: 1,
+//           justifyContent: 'center',
+//           alignItems: 'center',
+//           height: '100%',
+//         }}
+//       >
+//         <Result
+//           status="error"
+//           title="Error retrieving inbox"
+//           subTitle={error || 'Unknown error'}
+//         />
+//       </Flex>
+//     );
+//   }
 
-        <ConversationsPreviewList
-          loading={status === 'loading'}
-          conversations={conversations}
-          selectedConversationId={selectedConversationId}
-          hasMoreConversations={hasMoreConversations}
-          isConversationClosing={(conversationId) =>
-            closing.indexOf(conversationId) !== -1
-          }
-          onSelectConversation={handleSelectConversation}
-          onLoadMoreConversations={handleLoadMoreConversations}
-        />
-      </Sider>
+//   return (
+//     <Layout style={{background: colors.white}}>
+//       <Sider
+//         theme="light"
+//         width={CONVERSATIONS_DASHBOARD_SIDER_WIDTH}
+//         style={{
+//           borderRight: '1px solid #f0f0f0',
+//           overflow: 'auto',
+//           height: '100vh',
+//           position: 'fixed',
+//           left: CONVERSATIONS_DASHBOARD_SIDER_OFFSET,
+//         }}
+//       >
+//         <Box sx={{borderBottom: '1px solid #f0f0f0'}}>
+//           <Box px={3} py={3}>
+//             <Title level={3} style={{marginBottom: 0, marginTop: 8}}>
+//               {inbox.name}
+//             </Title>
+//           </Box>
+//         </Box>
 
-      <Layout
-        style={{
-          marginLeft: CONVERSATIONS_DASHBOARD_OFFSET,
-          background: colors.white,
-        }}
-      >
-        {conversation && (
-          <ConversationHeader
-            conversation={conversation}
-            users={users}
-            onAssignUser={handleAssignUser}
-            onMarkPriority={handleMarkPriority}
-            onRemovePriority={handleRemovePriority}
-            onCloseConversation={handleCloseConversation}
-            onReopenConversation={handleReopenConversation}
-            onDeleteConversation={handleDeleteConversation}
-          />
-        )}
-        {/* TODO: if no selected conversation, render something else */}
-        {conversation ? (
-          <SelectedConversationContainer
-            loading={status === 'loading'}
-            account={account}
-            currentUser={currentUser}
-            conversation={conversation}
-            isClosing={isClosingSelected}
-            setScrollRef={setScrollRef}
-            onSendMessage={handleSendNewMessage}
-          />
-        ) : null}
-      </Layout>
-    </Layout>
-  );
-};
+//         <ConversationsPreviewList
+//           loading={status === 'loading'}
+//           conversations={conversations}
+//           selectedConversationId={selectedConversationId}
+//           hasMoreConversations={hasMoreConversations}
+//           isConversationClosing={(conversationId) =>
+//             closing.indexOf(conversationId) !== -1
+//           }
+//           onSelectConversation={handleSelectConversation}
+//           onLoadMoreConversations={handleLoadMoreConversations}
+//         />
+//       </Sider>
+
+//       <Layout
+//         style={{
+//           marginLeft: CONVERSATIONS_DASHBOARD_OFFSET,
+//           background: colors.white,
+//         }}
+//       >
+//         {conversation && (
+//           <ConversationHeader
+//             conversation={conversation}
+//             users={users}
+//             onAssignUser={handleAssignUser}
+//             onMarkPriority={handleMarkPriority}
+//             onRemovePriority={handleRemovePriority}
+//             onCloseConversation={handleCloseConversation}
+//             onReopenConversation={handleReopenConversation}
+//             onDeleteConversation={handleDeleteConversation}
+//           />
+//         )}
+//         {/* TODO: if no selected conversation, render something else */}
+//         {conversation ? (
+//           <SelectedConversationContainer
+//             loading={status === 'loading'}
+//             account={account}
+//             currentUser={currentUser}
+//             conversation={conversation}
+//             isClosing={isClosingSelected}
+//             setScrollRef={setScrollRef}
+//             onSendMessage={handleSendNewMessage}
+//           />
+//         ) : null}
+//       </Layout>
+//     </Layout>
+//   );
+// };
 
 const Wrapper = (props: RouteComponentProps<{id: string}>) => {
   const {id: inboxId} = props.match.params;
