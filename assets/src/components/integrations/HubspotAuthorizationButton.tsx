a2004150e4862f2aaa3e4670cb585cbb0ebfa98c
reichert621 <reichertjalex@gmail.com>
2021-09-30 14:57:49 -0400

    Minor cleanup

--- assets/src/components/integrations/HubspotAuthorizationButton.tsx
+++ assets/src/components/integrations/HubspotAuthorizationButton.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import {Box, Flex} from 'theme-ui';
 import {Button, Popconfirm} from '../common';
-
-// const HUBSPOT_CLIENT_ID = '01ec4478-4828-43b5-b505-38f517856add';
-// const HUBSPOT_REDIRECT_URI = 'http://localhost:3000/integrations/hubspot';
-// const HUBSPOT_OAUTH_URL = `https://app.hubspot.com/oauth/authorize?client_id=${HUBSPOT_CLIENT_ID}&redirect_uri=${HUBSPOT_REDIRECT_URI}&scope=contacts%20content`;
-const HUBSPOT_OAUTH_URL =
-  'https://app.hubspot.com/oauth/authorize?client_id=01ec4478-4828-43b5-b505-38f517856add&redirect_uri=http://localhost:3000/integrations/hubspot&scope=contacts%20content';
+import {getHubspotAuthUrl} from './support';
 
 export const HubspotAuthorizationButton = ({
   isConnected,
@@ -17,16 +12,14 @@ export const HubspotAuthorizationButton = ({
   authorizationId?: string | null;
   onDisconnect: (id: string) => void;
 }) => {
+  const url = getHubspotAuthUrl();
+
   return (
     <>
       {authorizationId && isConnected ? (
         <Flex mx={-1}>
           <Box mx={1}>
-            <a
-              href={HUBSPOT_OAUTH_URL}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href={url} target="_blank" rel="noopener noreferrer">
               <Button>Reconnect</Button>
             </a>
           </Box>
@@ -43,7 +36,7 @@ export const HubspotAuthorizationButton = ({
           </Box>
         </Flex>
       ) : (
-        <a href={HUBSPOT_OAUTH_URL} target="_blank" rel="noopener noreferrer">
+        <a href={url} target="_blank" rel="noopener noreferrer">
           <Button>Connect</Button>
         </a>
       )}
